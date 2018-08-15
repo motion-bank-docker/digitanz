92ed2f9811fc27a657034c3023adf4731653c05a
christianrhansen <info@christianhansen.net>
2018-08-15 15:16:16 +0200

    mr griddle is dancing

--- src/pages/welcome.vue
+++ src/pages/welcome.vue
@@ -1,43 +1,25 @@
 <template lang="pug">
   .position-relative(style="height: calc(100vh - 50px); overflow-y: hidden;")
-    img(:src="changeImg(currentImg)")
-    // img(:src="images[0]")
+    img.vertical-middle.absolute(:src="currentImg", style="transform: scale(.7)")
 </template>
 
 <script>
   export default {
     data () {
       return {
-        currentImg: 1,
-        images: ['statics/003-master-1.svg', 'statics/003-master-2.svg', 'statics/003-master-3.svg']
+        currentImg: undefined,
+        images: ['statics/003-master-1.svg', 'statics/003-master-3.svg', 'statics/003-master-2.svg']
       }
     },
     mounted () {
-      // this.changeImg()
+      const _this = this
       let i = 0
+      this.currentImg = this.images[0]
       setInterval(function () {
-        if (i < 2) i++
+        if (i < _this.images.length - 1) i++
         else i = 0
-        this.currentImg = i
-        console.log(i)
-        return this.currentImg
+        _this.currentImg = _this.images[i]
       }, 1000)
-    },
-    methods: {
-      changeImg (val) {
-        console.log(val)
-        // let i = 0
-        // let imgs = this.images
-        /* setInterval(function () {
-          if (i < 2) i++
-          else i = 0
-          this.currentImg = i
-          // console.log(imgs[this.currentImg])
-          // return imgs[i]
-        }, 1000) */
-        return this.images[val]
-        // console.log(imgs[i])
-      }
     }
   }
 </script>
