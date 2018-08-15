86a4aafc61d3665c7fe2e1b3e40c317628c8bd9b
A. Koch <dasantonym@users.noreply.github.com>
2018-08-15 18:13:24 +0200

    comment out edit buttons

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -29,7 +29,7 @@
                 img(:src="item.preview", style="height: auto; max-height: 50vh; width: auto; max-width: 100%;")
             q-item-tile.no-margin.text-center.q-pt-sm
               q-btn(flat, round, :icon="getItemStyle(item).icon", :color="getItemStyle(item).color", @click="setAsPortrait(item)")
-              q-btn(flat, round, icon="edit")
+              // q-btn(flat, round, icon="edit")
               q-btn(flat, round, icon="delete", @click="openDeleteModal(item)")
               q-btn(flat, round, icon="cloud_download", @click="download(item.annotation.body.source.id)")
 </template>
