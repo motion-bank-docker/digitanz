f8e6df9f5741698bdb0b263944a5fba2a20f3a6f
A. Koch <dasantonym@users.noreply.github.com>
2018-08-23 22:45:20 +0200

    fix delete sequence and re-rendering workflows

--- src/lib/sequence-helper.js
+++ src/lib/sequence-helper.js
@@ -1,3 +1,5 @@
+import path from 'path'
+
 class SequenceHelper {
   static async renderSequence (context, timeline, targetTimeline = undefined, items = undefined) {
     if (!items) {
@@ -8,6 +10,10 @@ class SequenceHelper {
       items = results.items.sort(context.$sort.onRef).map(annotation => {
         return { annotation }
       })
+    }
+    if (!items.length) {
+      await SequenceHelper.deleteSequence(context, timeline.uuid)
+      return
     }
     const detail = {
       title: timeline.title,
@@ -25,6 +31,88 @@ class SequenceHelper {
     }
     const job = await context.$store.dispatch('sequences/post', { sequence, detail })
     console.debug('render sequence', job, detail, sequence)
+  }
+
+  static async deleteSequence (context, uuid) {
+    /**
+     * Delete map
+     */
+    const map = await context.$store.dispatch('maps/get', uuid)
+    try {
+      await context.$store.dispatch('maps/delete', map.uuid)
+    }
+    catch (e) {
+      console.error('Failed to remove annotation', e.message)
+      context.$captureException(e)
+    }
+
+    /**
+     * Delete associated annotations
+     */
+    let result = await context.$store.dispatch('annotations/find', {
+      'target.id': `${process.env.TIMELINE_BASE_URI}${map.uuid}`
+    })
+    for (let annotation of result.items) {
+      try {
+        await context.$store.dispatch('annotations/delete', annotation.uuid)
+      }
+      catch (e) {
+        console.error('Failed to remove annotation', e.message)
+        context.$captureException(e)
+      }
+    }
+
+    /**
+     * Build headers config for direct requests with axios
+     */
+    const headers = {
+      Authorization: `Bearer ${localStorage.getItem('access_token')}`
+    }
+    const mediaFile = `${process.env.ASSETS_BASE_PATH}${map.uuid}.mp4`
+
+    /**
+     * Delete associated references
+     */
+    result = await context.$store.dispatch('annotations/find', { 'body.source.id': mediaFile })
+    for (let annotation of result.items) {
+      try {
+        await context.$store.dispatch('annotations/delete', annotation.uuid)
+      }
+      catch (e) {
+        console.error('Failed to remove annotation', e.message)
+        context.$captureException(e)
+      }
+    }
+
+    /**
+     * Remove video previews
+     */
+    const preview = {
+      high: mediaFile.replace(/\.mp4$/, '.jpg'),
+      medium: mediaFile.replace(/\.mp4$/, '-m.jpg'),
+      small: mediaFile.replace(/\.mp4$/, '-s.jpg')
+    }
+    const previewKeys = Object.keys(preview)
+    for (let key of previewKeys) {
+      try {
+        await context.$axios.delete(`${process.env.TRANSCODER_HOST}/uploads/${path.basename(preview[key])}`, { headers })
+      }
+      catch (e) {
+        console.error('Failed to remove preview', e.message)
+        context.$captureException(e)
+      }
+    }
+
+    /**
+     * Remove video file
+     */
+    try {
+      await context.$axios.delete(`${process.env.TRANSCODER_HOST}/uploads/${path.basename(mediaFile)}`, { headers })
+    }
+    catch (e) {
+      console.error('Failed to remove video', e.message)
+      context.$captureException(e)
+    }
   }
 }
 
