d66f09cb6d07df3865c647a3b1e2492074425a9d
Florian Jenett <fjenett@gmail.com>
2018-08-30 18:00:30 +0200

    typo in text

--- src/i18n/de/index.js
+++ src/i18n/de/index.js
@@ -131,7 +131,7 @@ export default {
   navigation: {
     team: {
       label: 'Team',
-      sublabel: 'Meldet euch beu uns'
+      sublabel: 'Meldet euch bei uns'
     },
     login: 'Einloggen',
     logout: 'Ausloggen',
