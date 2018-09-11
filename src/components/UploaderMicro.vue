59243f0909c4acd5203beb732e36009a7d7212ec
christianrhansen <info@christianhansen.net>
2018-09-11 12:32:03 +0200

    created micro uploader component

---
+++ src/components/UploaderMicro.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+
+  q-uploader-extended-simple-micro(
+      ref="uploader",
+      :prefix="$t('buttons.uploader')",
+      auto-expand,
+      clearable,
+      color="primary",
+      align="center",
+      inverted,
+      :extensions="ext",
+      :multiple="false",
+      :url="url",
+      @add="onSelect",
+      @uploaded="onUploaded",
+      @fail="onFail",
+      @start="onStart",
+      @finish="onFinish",
+      @click="handleUploaderClick()")
+
+</template>
+
+<script>
+  import Uploader from './Uploader'
+  import QUploaderExtendedSimpleMicro from './QUploaderExtendedSimpleMicro'
+
+  export default {
+    extends: Uploader,
+    components: {
+      QUploaderExtendedSimpleMicro
+    }
+  }
+</script>
+
+<style lang="stylus">
+</style>
