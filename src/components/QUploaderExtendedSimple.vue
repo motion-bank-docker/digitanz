d383dc9ad832461513af7c1e1dff2d92c6da2801
christianrhansen <info@christianhansen.net>
2018-09-11 14:48:50 +0200

    code cleanup.

--- src/components/QUploaderExtendedSimple.vue
+++ src/components/QUploaderExtendedSimple.vue
@@ -1,26 +1,8 @@
 <template lang="pug">
   .q-uploader.relative-position.no-padding(:class="classes", @dragover="__onDragOver")
-    <!--q-input-frame(-->
-    <!--ref="input",-->
-    <!--:prefix="prefix",-->
-    <!--:suffix="suffix",-->
-    <!--:stackLabel="stackLabel",-->
-    <!--:floatLabel="floatLabel",-->
-    <!--:error="error",-->
-    <!--:warning="warning",-->
-    <!--:readonly="readonly",-->
-    <!--:inverted="inverted",-->
-    <!--:invertedLight="invertedLight",-->
-    <!--:dark="dark",-->
-    <!--:hideUnderline="hideUnderline",-->
-    <!--:before="before",-->
-    <!--:after="after",-->
-    <!--:color="color",-->
-    <!--:align="align",-->
-    <!--:noParentField="noParentField",-->
-    <!--:length="queueLength",-->
-    <!--:additionalLength="true")-->
-    q-input-frame(
+
+    // q-input-frame(
+    .round-borders.text-white.bg-primary(
     ref="input",
     :stackLabel="stackLabel",
     :floatLabel="floatLabel",
@@ -33,34 +15,26 @@
     :hideUnderline="hideUnderline",
     :before="before",
     :after="after",
-    :color="color",
+    :color="primary",
     :align="align",
     :noParentField="noParentField",
     :length="queueLength",
     :additionalLength="true")
-
-      // .col.q-input-target.ellipsis(:class="alignClass") {{label}}
 
       template(v-if="uploading")
         q-item
           q-item-side
             q-spinner.q-if-end.self-center(size="24px", color="white")
           q-item-main(:label="files[0].name", :sublabel="files[0].__size")
-          q-item-side.q-pr-sm(:right="true")
+          q-item-side(:right="true")
             q-item-tile.cursor-pointer.text-white(:icon="$q.icon.uploader[files[0].__doneUploading ? 'done' : 'clear']",
             :color="white",
             @click.native="__remove(files[0])",
             size="md")
-        //
-          q-icon.q-if-end.self-center.q-if-control(
-          slot="after",
-          // :name="$q.icon.uploader[`clear${this.isInverted ? 'Inverted' : ''}`]",
-          @click.native="abort")
 
       template(v-else)
-        // q-icon.full-width.q-uploader-pick-button.self-center.q-if-control.relative-position.overflow-hidden(
         q-btn.full-width(
-        :icon="$q.icon.uploader.add",
+        icon="arrow_upward",
         :disabled="addDisabled",
         flat)
 
@@ -71,40 +45,6 @@
           :accept="extensions",
           :multiple="multiple",
           @change="__add")
-        //
-          q-icon.q-if-control.self-center(
-          slot="after",
-          v-if="!hideUploadButton",
-          // :name="$q.icon.uploader.upload",
-          // :disabled="queueLength === 0",
-          @click.native="upload")
-
-      //
-        q-icon.q-if-control.generic_transition.self-center(
-        v-if="hasExpandedContent",
-        slot="after",
-        // :class="{'rotate-180': expanded}",
-        // :name="$q.icon.uploader.expand",
-        @click.native="expanded = !expanded")
-
-    //
-      q-slide-transition
-        div(:class="expandClass", :style="expandStyle", v-show="expanded")
-          q-list.q-uploader-files.q-py-none.scroll.no-border(:style="fileStyle", :dark="dark")
-            template(v-for="file in files")
-              // q-item.q-uploader-file.q-pa-xs(:key="file.name + file.__timestamp")
-              q-item.q-my-sm.shadow-3.q-uploader-file.q-pa-xs(:key="file.name + file.__timestamp")
-                template(v-if="!hideUploadProgress")
-                  q-item-side
-                    q-spinner.q-if-end.self-center(size="24px")
-                    //
-                      q-progress.q-uploader-progress-bg(
-                      //:percentage="file.__progress")
-                      .text-primary {{file.__progress}} %
-                // q-item-side(v-bind="file.__img ? { image: file.__img.src } : { icon: $q.icon.uploader.file, color: color }")
-                q-item-main(:label="file.name", :sublabel="file.__size")
-                q-item-side(:right="true")
-                  q-item-tile.cursor-pointer(:icon="$q.icon.uploader[file.__doneUploading ? 'done' : 'clear']", :color="color", @click.native="__remove(file)")
 
     template(v-if="dnd")
       .q-uploader-dnd.flex.row.items-center.justify-center.absolute-full(
