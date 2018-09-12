0da14dbee1787cc84e08dd7809f82f0f53e5b25c
Denis Klein <denisklein@users.noreply.github.com>
2018-09-12 15:33:35 +0200

    starting point

---
+++ src/pages/my-digitanz.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  q-page
+    p hello world
+</template>
+
+<script>
+  import VideoItem from '../../components/VideoItem'
+
+  export default {
+    components: {
+      VideoItem
+    },
+    data () {
+      return {
+        foo: false
+      }
+    },
+    mounted () {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped lang="stylus">
+</style>
