df9aa54fe5e00f4b3b8304dcafc00f250f7c1efe
christianrhansen <info@christianhansen.net>
2018-09-12 15:23:58 +0200

    added "my-digitanz", "tools", "dashboard-new"

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,7 +8,9 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/welcome') },
       { path: 'dashboard', component: () => import('pages/dashboard'), meta: {private: true} },
+      { path: 'dashboard-new', component: () => import('pages/dashboard-new'), meta: {private: true} },
       { path: 'allrecipes', component: () => import('pages/allRecipes'), meta: {private: true} },
+      { path: 'my-digitanz', component: () => import('pages/my-digitanz'), meta: {private: true} },
       { path: 'slotmachine', component: () => import('pages/slotMachine'), meta: {private: true} },
       { path: 'sequences', component: () => import('pages/sequences/list'), meta: {private: true} },
       { path: 'sequences/create', component: () => import('pages/sequences/edit'), meta: {private: true} },
@@ -20,6 +22,7 @@ const routes = [
       { path: 'groupsequences', component: () => import('pages/groupSequences'), meta: {private: true} },
       { path: 'portraitplusplus/responses/:uuid', component: () => import('pages/responses'), meta: {private: true, allowSelfResponse: true} },
       { path: 'team', component: () => import('pages/team'), meta: {private: true} },
+      { path: 'tools', component: () => import('pages/tools'), meta: {private: true} },
       { path: 'dk', component: () => import('pages/devtestdk'), meta: {private: true} },
       { path: 'mr-griddle', component: () => import('pages/mr-griddle'), meta: {private: true} },
       { path: 'space-tool', component: () => import('pages/space-tool'), meta: {private: true} },
