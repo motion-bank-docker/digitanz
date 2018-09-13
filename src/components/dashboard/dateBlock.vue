e6e62db0ddcd0f1acc5d802a21fc48b573a735e3
christianrhansen <info@christianhansen.net>
2018-09-13 17:19:03 +0200

    cleaned code

--- src/components/dashboard/dateBlock.vue
+++ src/components/dashboard/dateBlock.vue
@@ -1,18 +1,12 @@
 <template lang="pug">
   q-card.q-mb-md.q-mx-md
-    .row.q-mx-md
-      q-item.full-width.no-padding
-        q-item-main.no-padding
-          q-item-tile
-            slot(name="title")
-          q-item-tile
-            slot(name="description")
-        // q-item-side.no-padding.q-caption.text-right
-          slot(name="date")
-      slot(name="sublabel")
-    slot(name="content").bg-grey-4
-    // .row.q-ma-md
-      slot(name="buttons")
+    q-card-header
+      slot(name="title")
+
+    q-card-separator
+
+    q-card-main.q-pt-md.q-px-none.q-pb-none
+      slot(name="content")
 </template>
 
 <script>
