56547918b4831dfe04c1c19f59c841691c552097
ronjabu <ronjabutschbacher@gmx.de>
2018-09-13 11:44:39 +0200

    disabled upcoming tool-buttons

--- src/pages/tools.vue
+++ src/pages/tools.vue
@@ -13,22 +13,22 @@
         q-btn.q-ma-xs(@click.native="$router.push('/mr-griddle')", round, color="lime-5", size="xl")
           q-icon(name="accessibility", size="xl")
         p Form
-      .column.items-center.q-mb-xs.col-xs-6.col-md-2
-        q-btn.q-ma-xs(round, color="deep-orange-5", size="xl")
+      .column.items-center.q-mb-xs.col-xs-6.col-md-2(style="opacity: 0.2")
+        q-btn.q-ma-xs(round, color="deep-orange-5", size="xl", disabled)
           q-icon(name="timer", size="xl")
         p Rhytmus
-      .column.items-center.q-mb-xs.col-xs-6.col-md-2
-        q-btn.q-ma-xs(round, color="orange-5", size="xl")
+      .column.items-center.q-mb-xs.col-xs-6.col-md-2(style="opacity: 0.2")
+        q-btn.q-ma-xs(round, color="orange-5", size="xl", disabled)
           q-icon(name="group", size="xl")
         p Raum
-      .column.items-center.q-mb-xs.col-xs-6.col-md-2
-        q-btn.q-ma-xs(round, color="teal-5", size="xl")
-          q-icon(name="local_pizza", size="xl")
-        p Tool-5
-      .column.items-center.q-mb-xs.col-xs-6.col-md-2
-        q-btn.q-ma-xs(round, color="indigo-5", size="xl")
-          q-icon(name="local_florist", size="xl")
-        p Tool-6
+      .column.items-center.q-mb-xs.col-xs-6.col-md-2(style="opacity: 0.2")
+        q-btn.q-ma-xs(round, color="teal-5", size="xl", disabled)
+          q-icon(name="stars", size="xl")
+        p ...
+      .column.items-center.q-mb-xs.col-xs-6.col-md-2(style="opacity: 0.2")
+        q-btn.q-ma-xs(round, color="indigo-5", size="xl", disabled)
+          q-icon(name="stars", size="xl")
+        p ...
 </template>
 
 <script>
