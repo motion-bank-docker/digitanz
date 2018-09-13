121d8797679c3d61cdd8d11d50aaba68158c4856
ronjabu <ronjabutschbacher@gmx.de>
2018-09-13 16:57:48 +0200

    adjusted mini-uploadbutton

--- src/components/QUploaderExtendedSimpleMicro.vue
+++ src/components/QUploaderExtendedSimpleMicro.vue
@@ -6,8 +6,8 @@
 
     template(v-else)
       q-icon(
-      name="add",
-      :size="size"
+      name="add_circle",
+      :size="size",
       :disabled="addDisabled",
       :color="color"
       flat)
