348e8012d0bb51d32d67a585c20ea025fce7f6fc
ronjabu <ronjabutschbacher@gmx.de>
2018-09-13 13:01:46 +0200

    removed colors from tool-buttons

--- src/pages/tools.vue
+++ src/pages/tools.vue
@@ -2,31 +2,31 @@
   q-page
     q-list.no-border.q-mt-sm.row
       .column.items-center.q-mb-xs.col-12
-        q-btn.q-ma-xs(@click.native="$router.push('/upload')", round, color="amber-5", outline, size="xl")
-          q-icon(name="cloud_upload", outline, color="amber-5", size="xl")
+        q-btn.q-ma-xs(@click.native="$router.push('/upload')", round, outline, size="xl")
+          q-icon(name="cloud_upload", outline, size="xl")
         p Upload
       .column.items-center.q-mb-xs.col-xs-6.col-md-2
-        q-btn.q-ma-xs(@click.native="$router.push('/sequences')", round, color="light-green-5", size="xl")
+        q-btn.q-ma-xs(@click.native="$router.push('/sequences')", round, outline, size="xl")
           q-icon(name="extension", size="xl")
         p Choreographie
       .column.items-center.q-mb-xs.col-xs-6.col-md-2
-        q-btn.q-ma-xs(@click.native="$router.push('/mr-griddle')", round, color="lime-5", size="xl")
+        q-btn.q-ma-xs(@click.native="$router.push('/mr-griddle')", round, outline, size="xl")
           q-icon(name="accessibility", size="xl")
         p Form
       .column.items-center.q-mb-xs.col-xs-6.col-md-2(style="opacity: 0.2")
-        q-btn.q-ma-xs(round, color="deep-orange-5", size="xl", disabled)
+        q-btn.q-ma-xs(round, outline, size="xl", disabled)
           q-icon(name="timer", size="xl")
         p Rhytmus
       .column.items-center.q-mb-xs.col-xs-6.col-md-2(style="opacity: 0.2")
-        q-btn.q-ma-xs(round, color="orange-5", size="xl", disabled)
+        q-btn.q-ma-xs(round, outline, size="xl", disabled)
           q-icon(name="group", size="xl")
         p Raum
       .column.items-center.q-mb-xs.col-xs-6.col-md-2(style="opacity: 0.2")
-        q-btn.q-ma-xs(round, color="teal-5", size="xl", disabled)
+        q-btn.q-ma-xs(round, outline, size="xl", disabled)
           q-icon(name="stars", size="xl")
         p ...
       .column.items-center.q-mb-xs.col-xs-6.col-md-2(style="opacity: 0.2")
-        q-btn.q-ma-xs(round, color="indigo-5", size="xl", disabled)
+        q-btn.q-ma-xs(round, outline, size="xl", disabled)
           q-icon(name="stars", size="xl")
         p ...
 </template>
