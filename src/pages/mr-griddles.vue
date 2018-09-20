eb1550dce1a1c5d79060b3066513cd3784aacb9b
Florian Jenett <fjenett@gmail.com>
2018-09-20 16:27:28 +0200

    mr-griddle listing load data

--- src/pages/mr-griddles.vue
+++ src/pages/mr-griddles.vue
@@ -6,10 +6,41 @@
 
 <script>
   import MrGriddleListView from '../components/MrGriddleListView'
+  import { mapGetters } from 'vuex'
 
   export default {
     components: {
       MrGriddleListView
+    },
+    computed: {
+      ...mapGetters({
+        user: 'auth/getUserState'
+      })
+    },
+    watch: {
+      user (val) {
+        if (val) this.loadData()
+      }
+    },
+    methods: {
+      async loadData () {
+        const query = {
+          type: 'Timeline',
+          'author.id': this.user.uuid
+        }
+        const maps = await this.$store.dispatch('maps/find', query)
+        const griddleSequences = maps.items.filter(m => {
+          return m.title.indexOf('GriddleSequence ') === 0
+        })
+        let sequenceAnnotations = []
+        for (let seq of griddleSequences) {
+          const annotations = await this.$store.dispatch('annotations/find', {
+            'target.id': seq.id
+          })
+          sequenceAnnotations.push(annotations.items[0])
+        }
+        console.log(sequenceAnnotations)
+      }
     }
   }
 </script>
