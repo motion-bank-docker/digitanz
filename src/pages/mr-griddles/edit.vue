87e701d808f438149221fe7235c0dc8c97a0bddc
christianrhansen <info@christianhansen.net>
2018-10-04 16:32:48 +0200

    created "mr. griddles edit"

---
+++ src/pages/mr-griddles/edit.vue
@@ -0,0 +1,71 @@
+<template lang="pug">
+  q-page.row
+    mr-griddle#mr-griddle-container(ref="mrGriddleContainer"
+      :play="playing" @stateChanged="handleStateChanged" @editModeChanged="handleEditChange")
+    mr-griddle-handler.fixed-bottom(
+    v-if="!editMode"
+      :play="playing"
+        :states="storedStates"
+        :currentState="currentState"
+        @clickPlay="handleClickPlay"
+        @clickAdd="handleClickAdd"
+        @clickState="handleClickState"
+        @deleteItem="handleDeleteItem"
+        @saveSequence="handeSaveSequence")
+</template>
+
+<script>
+  import MrGriddle from '../../components/MrGriddle'
+  import MrGriddleHandler from '../../components/MrGriddleHandler'
+  export default {
+    components: {
+      MrGriddle,
+      MrGriddleHandler
+    },
+    data () {
+      return {
+        playing: false,
+        editMode: false,
+        storedStates: [],
+        currentState: -1
+      }
+    },
+    methods: {
+      handleClickPlay () {
+        this.playing = !this.playing
+      },
+      handleClickAdd () {
+        const _griddle = this.$refs.mrGriddleContainer
+        _griddle.handleStoreState()
+        this.storedStates = _griddle.storedStates
+      },
+      handleStateChanged (state) {
+        if (state === -1) this.playing = false
+        const _griddle = this.$refs.mrGriddleContainer
+        this.storedStates = _griddle.storedStates
+        this.currentState = state
+      },
+      handleEditChange (val) {
+        this.editMode = val
+      },
+      handleDeleteItem (item) {
+        const _griddle = this.$refs.mrGriddleContainer
+        _griddle.handleRemoveStoredState(item.index)
+      },
+      handleClickState (item) {
+        const _griddle = this.$refs.mrGriddleContainer
+        _griddle.setCurrentState(item.index)
+      },
+      handeSaveSequence () {
+        const _griddle = this.$refs.mrGriddleContainer
+        _griddle.saveSequence()
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  #mr-griddle-container
+    width 100vw
+    height calc(100vh - 120px)
+</style>
