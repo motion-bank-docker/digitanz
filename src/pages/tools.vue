f7679ef547913a2be6228f0773e96b69a6ff7bc8
christianrhansen <info@christianhansen.net>
2018-10-08 11:50:48 +0200

    code clean up

--- src/pages/tools.vue
+++ src/pages/tools.vue
@@ -1,33 +1,59 @@
 <template lang="pug">
   q-page
-    q-list.no-border.q-mt-md.row
-      .column.items-center.q-mb-xs.col-xs-6.col-md-2
-        q-btn.q-ma-xs(@click.native="$router.push('/sequences/create')", color="grey-9", round, size="xl")
-          q-icon(name="extension", size="xl")
-        p Sequenz
-      .column.items-center.q-mb-xs.col-xs-6.col-md-2
-        q-btn.q-ma-xs(@click.native="$router.push('/mr-griddle')", color="grey-9", round, size="xl")
-          q-icon(name="accessibility", size="xl")
-        p Form
-      .column.items-center.q-mb-xs.col-xs-6.col-md-2(style="opacity: 0.2")
-        q-btn.q-ma-xs(round, size="xl", disabled)
-          q-icon(name="timer", size="xl")
-        p Rhytmus
-      .column.items-center.q-mb-xs.col-xs-6.col-md-2(style="opacity: 0.2")
-        q-btn.q-ma-xs(round, size="xl", disabled)
-          q-icon(name="group", size="xl")
-        p Raum
-      .column.items-center.q-mb-xs.col-xs-6.col-md-2(style="opacity: 0.2")
-        q-btn.q-ma-xs(round, size="xl", disabled)
-          q-icon(name="stars", size="xl")
-        p ...
-      .column.items-center.q-mb-xs.col-xs-6.col-md-2(style="opacity: 0.2")
-        q-btn.q-ma-xs(round, size="xl", disabled)
-          q-icon(name="stars", size="xl")
-        p ...
+    .q-mt-md.row
+      .column.items-center.q-mb-xs.col-xs-6.col-md-2(v-for="button in buttons", :class="{'inactive': !button.status}")
+        q-btn.q-ma-xs(@click.native="actions(button.action)", :class="{'bg-grey-9': button.status}",
+        :disabled="!button.status", round, size="xl")
+          q-icon(:name="button.icon", size="xl")
+        p {{ button.label }}
 </template>
 
 <script>
   export default {
+    data () {
+      return {
+        buttons: [{
+          action: 'sequenz',
+          icon: 'extension',
+          label: 'Sequenz',
+          status: true
+        }, {
+          action: 'form',
+          icon: 'accessibility',
+          label: 'Form',
+          status: true
+        }, {
+          action: 'unset',
+          icon: 'stars',
+          label: 'Raum',
+          status: false
+        }, {
+          action: 'unset',
+          icon: 'stars',
+          label: 'Rhytmus',
+          status: false
+        }]
+      }
+    },
+    methods: {
+      actions (val) {
+        switch (val) {
+        case 'sequenz':
+          this.$router.push('/sequences/create')
+          break
+        case 'form':
+          this.$router.push('/mr-griddle')
+          break
+        case 'unset':
+          console.log('unset')
+          break
+        }
+      }
+    }
   }
 </script>
+
+<style scoped lang="stylus">
+  .inactive
+    opacity .2
+</style>
