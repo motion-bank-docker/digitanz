5683199648364717204bf396d9ca9ff5cd9f27ad
christianrhansen <info@christianhansen.net>
2018-10-08 18:16:55 +0200

    added mr. griddles to profiles

--- src/components/profil/UserMrGriddles.vue
+++ src/components/profil/UserMrGriddles.vue
@@ -1,10 +1,29 @@
 <template lang="pug">
   div
-    | inhalt
+    confirm-modal(ref="confirmDeleteModal", @confirm="deleteItem")
+
+    mr-griddle-list-view(
+    v-if="sequences",
+    layout-style='sm',
+    :items="sequences")
+
+      template(slot="customButtons" slot-scope="{ item }")
+        q-btn(flat, size="sm" round,
+        :icon="getItemStyle(item).icon", :color="getItemStyle(item).color",
+        @click="toggleItemFavorite(item)")
+
+        q-btn(flat, size="sm" round, icon="edit",
+        @click="$router.push(`/mr-griddle/${item.target.id.split('/').pop()}/edit`)")
+
+        q-btn(flat, size="sm" round, icon="delete",
+        @click="openDeleteModal(item)")
 </template>
 
 <script>
+  import allMrGriddles from '../dashboard/allMrGriddles'
+
   export default {
+    extends: allMrGriddles
   }
 </script>
 
