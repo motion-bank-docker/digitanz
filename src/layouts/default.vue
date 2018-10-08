466c01c26c504f98cb8334267d5212b9fe06b87f
christianrhansen <info@christianhansen.net>
2018-10-08 15:52:14 +0200

    code cleanup

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,102 +1,21 @@
 <template lang="pug">
   q-layout(view='lHh Lpr lFf')
-    q-layout-header
-      // q-toolbar(dark, color='black', :glossy="$q.theme === 'mat'", :inverted="$q.theme === 'ios'")
-        //
-          q-btn(v-if="userState", flat, dense, round, @click.native.prevent='leftDrawerOpen = !leftDrawerOpen', aria-label='Menu')
-            q-icon(name='menu')
-        q-toolbar-title
-          span(@click.prevent="clickTitle()") {{ info.productName }}
-        q-btn(color="primary", flat, icon-right="eject",
-          v-if="userState", @click.prevent="logout", :label="$t('navigation.logout')")
-        q-btn(color="primary", flat, icon-right="arrow_forward",
-          v-if="!userState", @click.prevent="login", :label="$t('navigation.login')")
-    // q-layout-drawer(dark, v-model='leftDrawerOpen', :content-class="$q.theme === 'mat' ? 'bg-dark' : null", v-if="userState")
-      //q-list(dark, no-border, link, inset-delimiter, v-if="urls")
-      q-list(dark, no-border, link, inset-delimiter)
-        // q-list-header Main Menu
-        q-item(@click.native="$router.push('/dashboard')")
-          q-item-side(icon="list")
-          q-item-main(:label='$t(\'navigation.dashboard.label\')', :sublabel='$t(\'navigation.dashboard.sublabel\')')
-        //
-          q-item
-            q-item-side
-            q-item-main
-              q-list.no-border.no-margin
-                // q-item.no-padding(@click.native="$router.push('/dashboard')") 17.8.2018
-                q-item.no-padding(@click.native="$router.push('/dashboard?item_id=abc')") 17.8.2018
-        q-item(@click.native="$router.push('/portrait')")
-          q-item-side(icon='account_box')
-          q-item-main(:label='$t(\'navigation.portrait.label\')', :sublabel='$t(\'navigation.portrait.sublabel\')')
 
-        // q-item(@click.native="$router.push('/portraitplusplus')")
-          q-item-side(icon='account_box')
-          q-item-main(:label='$t(\'navigation.portraitplusplus.label\')', :sublabel='$t(\'navigation.portraitplusplus.sublabel\')')
-
-        q-item(@click.native="$router.push('/groupsequences')")
-          q-item-side(icon='account_box')
-          q-item-main(:label="$t('navigation.groupsequences.label')", :sublabel="$t('navigation.groupsequences.sublabel')")
-
-        q-item(v-if="env.FEATURE_MR_GRIDDLE", @click.native="$router.push('/mr-griddle')", separator)
-          q-item-side(icon='accessibility')
-          // q-item-main(label='Mr. Griddle', sublabel='Do the griddle')
-          q-item-main(:label='$t(\'navigation.mr_griddle.label\')', :sublabel='$t(\'navigation.mr_griddle.sublabel\')')
-
-        q-item(v-if="env.FEATURE_RECIPES", @click.native="$router.push('/allrecipes')", separator)
-          q-item-side(icon='list')
-          // q-item-main(label='Rezepte', sublabel='Remix Motion-Recipes')
-          q-item-main(:label='$t(\'navigation.recipe.label\')', :sublabel='$t(\'navigation.recipe.sublabel\')')
-
-        q-item(v-if="env.FEATURE_SPACE", @click.native="urlClick('/lost-in-space')", separator)
-          q-item-side(icon='explore')
-          // q-item-main(label='Lost in Space', sublabel='Experience space')
-          q-item-main(:label='$t(\'navigation.lost_in_space.label\')', :sublabel='$t(\'navigation.lost_in_space.sublabel\')')
-
-        q-item(v-if="env.FEATURE_SEQUENCER", @click.native="$router.push('/sequences')", separator)
-          q-item-side(icon='movie')
-          q-item-main(:label="$t('navigation.video_sequencer.label')", :sublabel="$t('navigation.video_sequencer.sublabel')")
-
-        q-item(v-if="env.FEATURE_UPLOAD", @click.native="$router.push('/upload')", separator)
-          q-item-side(icon='cloud upload')
-          q-item-main(:label='$t(\'navigation.upload.label\')', :sublabel='$t(\'navigation.upload.sublabel\')')
-
-        q-item(@click.native="$router.push('/team')", separator)
-          q-item-side(icon='help')
-          q-item-main(:label="$t('navigation.team.label')", :sublabel="$t('navigation.team.sublabel')")
     q-layout-footer.text-center.bg-dark.q-py-xs(v-if="userState")
 
       template(v-if="hideBackButton")
-        // q-btn.q-ml-md(v-for="path in rootPaths", round, flat, size="lg",
-          // :class="{ 'text-white bg-grey-9': currentApp === path }",
-          @click="executeApp(path)")
-          q-icon(name="home")
+        q-btn.q-ml-md.q-mr-md(
+        v-for="button in buttons", round, flat, size="lg",
+        :class="{ 'text-white bg-grey-9': currentApp === button.action }",
+        @click="executeApp(button.action)")
+          q-icon(:name="button.icon")
 
-        q-btn(round, flat, size="lg",
-        :class="{ 'text-white bg-grey-9': currentApp === 'profil' }",
-        @click="executeApp('profil')")
-          q-icon(name="person")
-
-        q-btn.q-ml-md(round, flat, size="lg",
-        :class="{ 'text-white bg-grey-9': currentApp === 'dashboard-new' }",
-        @click="executeApp('dashboard-new')")
-          q-icon(name="people")
-
-        // q-btn.q-mx-md(@click="$router.push('tools')", round, flat, size="lg")
-        q-btn.q-ml-md.q-mr-md(round, flat, size="lg",
-        :class="{ 'text-white bg-grey-9': currentApp === 'tools' }",
-        @click="executeApp('tools')")
-          q-icon(name="build")
-        // q-btn.q-mx-md(@click="$router.push('tools')", round, flat, size="lg")
-
-        span.q-py-md.bg-grey-
-          // q-uploader-extended-simple-micro(round, flat, size="lg", outline, color="grey-5")
+        span.q-py-md
           file-uploader-micro
 
       template(v-else)
         q-btn.q-mx-md(round, flat, size="lg",
         @click="$router.go(-1)")
-          // q-icon.rotate-90(name="local_pizza")
-          // q-icon.rotate-45(name="call_received")
           q-icon.rotate-270(name="navigation")
 
     q-page-container
@@ -122,30 +41,23 @@
     },
     data () {
       return {
+        buttons: [{
+          action: 'profil',
+          icon: 'person',
+          status: true
+        }, {
+          action: 'dashboard-new',
+          icon: 'people',
+          status: true
+        }, {
+          action: 'tools',
+          icon: 'build',
+          status: true
+        }],
         rootPaths: ['', 'profil', 'dashboard-new', 'tools'],
-        /* rootPaths: [{
-          path: 'my-digitanz',
-          icon: 'home'
-        }, {
-          path: 'dashboard-new',
-          icon: 'person'
-        }, {
-          path: 'tools',
-          icon: 'explore'
-        }], */
         hideBackButton: true,
         currentApp: null,
         env: process.env
-        /* info: {
-          productName: '#digitanz'
-        }, */
-        // leftDrawerOpen: this.$q.platform.is.desktop,
-        // pathLength: this.$router.currentRoute.split('/'),
-        /* showFullNav: [
-          'tools',
-          'dashboard-new',
-          'my-digitanz'
-        ] */
       }
     },
     computed: {
@@ -157,14 +69,25 @@
       $route (to, from) {
         console.log(to, from)
         this.hideBackButton = this.rootPaths.indexOf(to.fullPath.substr(1)) > -1
-        // this.hideBackButton = this.rootPaths.map(x => x.path.indexOf(to.fullPath.substr(1)) > -1)
-        /* this.rootPaths.map(x => {
-          // console.log('#++##++#', x.path)
-          if (x.path.indexOf(to.fullPath.substr(1)) > -1) console.log(x)
-        }) */
       }
     },
     methods: {
+      actions (val) {
+        switch (val) {
+        case 'profil':
+          this.executeApp('profil')
+          break
+        case 'dashboard-new':
+          this.executeApp('dashboard-new')
+          break
+        case 'tools':
+          this.executeApp('tools')
+          break
+        case 'unset':
+          console.log('unset')
+          break
+        }
+      },
       clickTitle () {
         this.$router.push('/')
       },
@@ -185,6 +108,4 @@
 </script>
 
 <style scoped lang="stylus">
-  .border-left
-    border-left 1px solid #666
 </style>
