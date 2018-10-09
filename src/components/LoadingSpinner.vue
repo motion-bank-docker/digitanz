cefddf6ca35bba9fa07bec47e47b51450bbea45d
christianrhansen <info@christianhansen.net>
2018-10-09 18:07:56 +0200

    created "loading spinner" component

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  q-spinner(:size="spinnerSize")
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        spinnerSize: 30
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
