3e3076f392846340d9cedb4cea20c4dc71bdc7ad
Denis Klein <denisklein@users.noreply.github.com>
2018-10-10 16:42:35 +0200

    minor changes

--- src/components/dashboard/PublicUploads.vue
+++ src/components/dashboard/PublicUploads.vue
@@ -2,7 +2,7 @@
   div
     video-modal(ref="videoModal")
 
-    .text-center.q-mb-md(v-if="publicUploads.length <= 0")
+    .text-center.q-mb-md(v-if="publicUploads.length <= 0 && isLoading")
       loading-spinner
 
     .row.q-mx-md
@@ -31,7 +31,8 @@
     data () {
       return {
         publicUploadsMapUUID: `${process.env.TIMELINE_BASE_URI}${process.env.PUBLIC_UPLOADS_TIMELINE_UUID}`,
-        publicUploads: []
+        publicUploads: [],
+        isLoading: false
       }
     },
     async mounted () {
@@ -51,6 +52,7 @@
     },
     methods: {
       async loadData () {
+        this.isLoading = true
         this.$q.loading.show({ message: this.$t('messages.loading_sequences') })
         const query = {
           'target.id': this.publicUploadsMapUUID
@@ -58,6 +60,7 @@
         this.publicUploads = await VideoHelper.fetchVideoItems(this, query)
 
         this.$q.loading.hide()
+        this.isLoading = false
       }
     }
   }
