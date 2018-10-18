8981ae6ea7a308d0a0016cf248dbad711784c000
Denis Klein <denisklein@users.noreply.github.com>
2018-10-18 21:16:27 +0200

    UsersPublicMrGriddles

--- src/components/profil/UsersPublicMrGriddles.vue
+++ src/components/profil/UsersPublicMrGriddles.vue
@@ -1,23 +1,15 @@
 <template lang="pug">
 
   div
-
-    confirm-modal(ref="confirmDeleteModal", @confirm="deleteItem")
-
     mr-griddle-list-view(
     v-if="sequences.length > 0",
     layout-style='sm',
     :buttons="['more-delete']",
     :buttonsNew="buttonsNew",
     :buttonsNewDropdown="buttonsNewDropdown",
-    :items="sequences",
-    @emitLoadData="emitLoadData")
+    :items="sequences")
       template(slot="customButtons" slot-scope="{ video }")
-        q-btn(flat, size="sm", round, icon="people", color="grey-5", @click="toggleItemFavorite(video)")
-
       template(slot="customMoreButtons" slot-scope="{ video }")
-        q-item.q-px-sm
-          q-btn(round, flat, size="sm", icon="edit", @click="$router.push(`/sequences/${video.map.uuid}/edit`)")
 
     .text-center(v-else)
       loading-spinner
@@ -26,15 +18,31 @@
 
 <script>
   import LoadingSpinner from '../LoadingSpinner'
-  import MrGriddles from '../MrGriddles'
+  import { mapGetters } from 'vuex'
+  import MrGriddleListView from '../MrGriddleListView'
 
   export default {
-    extends: MrGriddles,
     components: {
-      LoadingSpinner
+      LoadingSpinner,
+      MrGriddleListView
+    },
+    mounted () {
+      this.loadData()
+    },
+    computed: {
+      ...mapGetters({
+        user: 'auth/getUserState'
+      })
+    },
+    watch: {
+      user (val) {
+        if (val) this.loadData()
+      }
     },
     data () {
       return {
+        sequences: [],
+        favoriteSequences: [],
         buttonsNew: [{
           icon: 'people',
           label: 'visibility'
@@ -49,8 +57,27 @@
       }
     },
     methods: {
-      emitLoadData () {
-        this.loadData()
+      async loadData () {
+        const map = await this.$store.dispatch('maps/get', process.env.MR_GRIDDLE_SEQUENCES_TIMELINE_UUID)
+        const annotations = await this.$store.dispatch('annotations/find', {
+          'target.id': map.id,
+          'body.purpose': 'linking',
+          'author.id': this.user.uuid
+        })
+        const sequenceIds = annotations.items.map(a => {
+          return a.body.source.id.split('/').pop()
+        })
+        let sequences = []
+        for (let seqId of sequenceIds) {
+          try {
+            const seqMap = await this.$store.dispatch('maps/get', seqId)
+            sequences.push(seqMap)
+          }
+          catch (e) {
+            // TODO fix orphaned annotations
+          }
+        }
+        this.sequences = sequences
       }
     }
   }
