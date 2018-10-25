d9699ee82753d03cf30c372a391e5c0952d769e4
christianrhansen <info@christianhansen.net>
2018-10-25 17:36:56 +0200

    show content

--- src/components/dashboard/PublicClouds.vue
+++ src/components/dashboard/PublicClouds.vue
@@ -6,32 +6,28 @@
     :items="associations",
     @emitLoadData="loadData()"
     )
+
 </template>
 
 <script>
   import { mapGetters } from 'vuex'
-  import { VideoHelper } from '../../lib/video-helper'
+
   import CloudListView from '../CloudListView'
 
   export default {
     components: {
+      // MrGriddleModal
+      // ConfirmModal
+      // VideoListView
       CloudListView
     },
     data () {
       return {
         associations: [],
         buttonsX: [{
-          icon: 'cloud_download',
-          label: 'download'
-        }],
-        /* buttonsY: [{
-          icon: 'edit',
-          label: 'edit'
-        }, {
-          icon: 'delete',
-          label: 'delete'
-        }], */
-        myAssociations: []
+          icon: 'chat',
+          label: 'response'
+        }]
       }
     },
     props: {
@@ -50,49 +46,16 @@
       }
     },
     methods: {
-      async deleteItem (uuid) {
-        await this.$store.dispatch('cloud/removeAssociation', uuid)
-        this.loadData()
-      },
       async loadData () {
-        this.myAssociations = []
+        // this.myAssociations = []
         if (!this.user) return
         this.$q.loading.show({ message: this.$t('messages.loading_data') })
-        const query = {
-          'author.id': this.user.uuid
-        }
-        this.associations = await this.$store.dispatch('cloud/listAssociations', query)
-        /*
-        this.associations.map(a => {
-          if (a.author.id === this.user.uuid) {
-            this.myAssociations.push(a)
-          }
-        })
-         */
-        console.log('loaded associations', this.associations)
-        console.debug('loaded associations', this.associations)
+        this.associations = await this.$store.dispatch('cloud/listPublicAssociations')
         this.$q.loading.hide()
-      },
-      async loadDataARCHIVE () {
-        // console.log('this.user.id', this.user.uuid)
-        const archiveMapResult = await this.$store.dispatch('maps/get', process.env.ARCHIVE_TIMELINE_UUID)
-        // console.log('archiveMapResult: ', archiveMapResult)
-        if (archiveMapResult) {
-          const archiveQuery = {
-            'target.id': `${process.env.TIMELINE_BASE_URI}${archiveMapResult.uuid}`
-          }
-          let archive = await VideoHelper.fetchVideoItems(this, archiveQuery)
-          console.log('archive', archive)
-          this.archiveItems = archive[0]
-        }
-      },
-      togglePublicity (val) {
-        console.log(val)
       }
     }
   }
 </script>
 
-<style scoped lang="stylus">
-  @import '~variables'
+<style scoped>
 </style>
