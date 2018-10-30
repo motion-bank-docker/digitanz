f203f8990bc39e0313b9c8f2ae05f8af0bdcce31
ronjabu <ronjabutschbacher@gmx.de>
2018-10-30 11:41:57 +0100

    pulled master into branch

--- src/pages/recipe.vue
+++ src/pages/recipe.vue
@@ -1,30 +1,24 @@
 <template lang="pug">
-  q-page
-    div
-      q-btn(
-      @click="$router.push('/allrecipes')", round, color="primary", icon="arrow back")
+   q-page
+    q-btn.float-right(v-if="!editMode", @click="editMode = true", round, color="primary", icon="edit")
+    q-input.q-title.q-mt-lg.q-mx-lg(v-if="editMode", dark, v-model="newRecipe.title", placeholder="Titel")
 
-      q-btn.float-right(v-if="!editMode", @click="editMode = true", round, color="primary", icon="edit")
+    h2.q-display-1.q-ml-lg(v-else) {{newRecipe.title}}
 
-    div.items-start
-      q-input.q-title.q-mt-lg.q-mx-lg(v-if="editMode", dark, v-model="newRecipe.title", placeholder="Titel")
+    q-list.q-ml-lg(v-if="newRecipe.entries.length > 0", no-border, v-dragula="newRecipe.entries", style="touch-action:none", drake="furz")
+      q-item.items-baseline(v-for="(ingr, index) in newRecipe.entries", :description="ingr", :key="ingr")
+        q-item-main
+          p.q-title {{ ingr }}
+        q-item-side
+          q-btn(v-if="editMode", @click="deleteTodoItem(index)", icon="delete")
 
-      h2.q-display-1.q-ml-lg(v-else) {{newRecipe.title}}
-
-      q-list.q-ml-lg(v-if="newRecipe.entries.length > 0", no-border, v-dragula="newRecipe.entries", style="touch-action:none", drake="furz")
-        q-item.items-baseline(v-for="(ingr, index) in newRecipe.entries", :description="ingr", :key="ingr")
-          q-item-main
-            p.q-title {{ ingr }}
-          q-item-side
-            q-btn(v-if="editMode", @click="deleteTodoItem(index)", icon="delete")
-
-    div(v-if="editMode")
-      q-input.q-ml-lg.q-mr-lg(dark, v-model="addIngedient", type="textarea",
-        v-on:keyup.enter="addTodoItem", placeholder="Rezeptpunkt hinzufügen")
-        q-btn.vertical-top(@click="addTodoItem") hinzufügen
-      div.justify-around.row
-        q-btn.q-mt-sm.col-4(@click="saveRecipe", color="primary") speichern
-        // q-btn.q-mt-sm.col-4(@click="remixArray", color="secondary") remix
+  div(v-if="editMode")
+    q-input.q-ml-lg.q-mr-lg(dark, v-model="addIngedient", type="textarea",
+      v-on:keyup.enter="addTodoItem", placeholder="Rezeptpunkt hinzufügen")
+      q-btn.vertical-top(@click="addTodoItem") hinzufügen
+    div.justify-around.row
+      q-btn.q-mt-sm.col-4(@click="saveRecipe", color="primary") speichern
+      // q-btn.q-mt-sm.col-4(@click="remixArray", color="secondary") remix
 </template>
 
 <script>
--- src/pages/recipes/create.vue
+++ src/pages/recipes/create.vue
@@ -1,31 +1,25 @@
 <template lang="pug">
   q-page
-    | test bhjakjdaskj
-    div
-      q-btn(
-      @click="$router.push('/allrecipes')", round, color="primary", icon="arrow back")
+    q-btn.float-right(v-if="!editMode", @click="editMode = true", round, color="primary", icon="edit")
+    q-input.q-title.q-mt-lg.q-mx-lg(v-if="editMode", dark, v-model="newRecipe.title", placeholder="Titel")
+    h2.q-display-1.q-ml-lg(v-else) {{newRecipe.title}}
+    q-list.q-ml-lg(v-if="newRecipe.entries.length > 0", no-border, v-dragula="newRecipe.entries", style="touch-action:none", drake="furz")
+      q-item.items-baseline(v-for="(ingr, index) in newRecipe.entries", :description="ingr", :key="ingr")
+        q-item-main
+          p.q-title {{ ingr }}
+        q-item-side
+          q-btn(v-if="editMode", @click="deleteTodoItem(index)", icon="delete")
 
-      q-btn.float-right(v-if="!editMode", @click="editMode = true", round, color="primary", icon="edit")
-
-    div.items-start
-      q-input.q-title.q-mt-lg.q-mx-lg(v-if="editMode", dark, v-model="newRecipe.title", placeholder="Titel")
-
-      h2.q-display-1.q-ml-lg(v-else) {{newRecipe.title}}
-
-      q-list.q-ml-lg(v-if="newRecipe.entries.length > 0", no-border, v-dragula="newRecipe.entries", style="touch-action:none", drake="furz")
-        q-item.items-baseline(v-for="(ingr, index) in newRecipe.entries", :description="ingr", :key="ingr")
-          q-item-main
-            p.q-title {{ ingr }}
-          q-item-side
-            q-btn(v-if="editMode", @click="deleteTodoItem(index)", icon="delete")
-
-    div(v-if="editMode")
-      q-input.q-ml-lg.q-mr-lg(dark, v-model="addIngedient", type="textarea",
-      v-on:keyup.enter="addTodoItem", placeholder="Rezeptpunkt hinzufügen")
-        q-btn.vertical-top(@click="addTodoItem") hinzufügen
-      div.justify-around.row
-        q-btn.q-mt-sm.col-4(@click="saveRecipe", color="primary") speichern
-        // q-btn.q-mt-sm.col-4(@click="remixArray", color="secondary") remix
+    q-list(v-if="editMode")
+      q-item.no-padding
+        q-item-main
+          q-input.q-ml-lg.q-mr-lg(hide-underline, dark, v-model="addIngedient", type="textarea", v-on:keyup.enter="addTodoItem", placeholder="Rezeptpunkt hinzufügen")
+        q-item-side
+          q-btn(@click="addTodoItem") hinzufügen
+      q-item-separator
+    div.justify-around.row
+      q-btn.q-mt-sm.col-4(@click="saveRecipe", color="primary") speichern
+      // q-btn.q-mt-sm.col-4(@click="remixArray", color="secondary") remix
 </template>
 
 <script>
