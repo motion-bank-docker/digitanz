0561b2e87f43be8dd3b22d268ef71842472ff433
Denis Klein <denisklein@users.noreply.github.com>
2018-11-07 12:39:53 +0100

    implement recipes in profile page

---
+++ src/components/RecipeListView.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .row.justify-between
+    recipe-item(
+    v-for="item in items",
+    :buttonsX="buttonsX",
+    :buttonsY="buttonsY",
+    :class="[size === 'sm' ? 'width-sm' : 'full-width']",
+    :showContentFlag="showContentFlag",
+    :item="item"
+    )
+</template>
+
+<script>
+  import RecipeItem from './RecipeItem'
+
+  export default {
+    components: {
+      RecipeItem
+    },
+    props: {
+      buttonsX: Array,
+      buttonsY: Array,
+      items: Array,
+      size: undefined,
+      showContentFlag: Boolean
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .width-sm
+    width: 46%
+</style>
