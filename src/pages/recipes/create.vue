d5abaa2fd3525d629726a874e966a8b6400a1bb6
Denis Klein <denisklein@users.noreply.github.com>
2018-11-08 15:53:48 +0100

    styling create/view recipe page

--- src/pages/recipes/create.vue
+++ src/pages/recipes/create.vue
@@ -4,10 +4,11 @@
     q-input.q-title.q-my-md(dark, v-model="newRecipe.title", placeholder="Titel", :error="$v.newRecipe.title.$error",
     :readonly="!editMode", :hide-underline="!editMode")
     // h2.q-display-1.q-ml-lg(v-else) {{newRecipe.title}}
-    q-list.q-ml-lg(v-if="newRecipe.entries.length > 0", no-border)
+    q-list(v-if="newRecipe.entries.length > 0", no-border, separator, dark, :sparse="!editMode")
       q-item.items-baseline(v-for="(ingr, index) in newRecipe.entries", :description="ingr", :key="ingr")
+        q-item-side(v-if="!editMode") {{ index + 1 }}
         q-item-main
-          p.q-title.word-break {{ ingr }}
+          p(:class="!editMode ? 'q-title' : ''").q-mb-none.word-break {{ ingr }}
         q-item-side(v-if="editMode")
           q-btn(icon="keyboard_arrow_up", @click="moveUp(index)")
           q-btn(icon="keyboard_arrow_down", @click="moveDown(index)")
