7d9f1e7b9fd9fe480e2d81d7632449eed4ce246c
Anton <antonymousfeedback@gmail.com>
2019-07-10 13:15:17 +0200

    Turn into debug stub

--- src/store/modules/cloud.js
+++ src/store/modules/cloud.js
@@ -1,64 +1,33 @@
-import axios from 'axios'
-import { ObjectUtil } from 'mbjs-utils'
-
-const getRequestConfig = () => {
-  return {
-    headers: {
-      Authorization: `Bearer ${localStorage.access_token}`
-    }
-  }
-}
-
-const baseURL = `${process.env.DIGITANZ_API_HOST}/objects`
-
 const cloud = {
   namespaced: true,
   state: {},
   actions: {
     async addWord (context, word) {
-      const result = await axios.post(baseURL, {
-        type: 'word',
-        value: word
-      }, getRequestConfig())
-      return result.data
+      console.debug('addWord', word)
     },
     async listWords () {
-      const query = JSON.stringify({ type: 'word' })
-      const result = await axios.get(baseURL, ObjectUtil.merge(getRequestConfig(), { params: { query } }))
-      return result.data ? result.data.items : []
+      console.debug('listWords')
+      return []
     },
     async addAssociation (context, words) {
-      const result = await axios.post(baseURL, {
-        type: 'word-association',
-        value: words
-      }, getRequestConfig())
-      return result.data
+      console.debug('addAssociation', words)
     },
     async listAssociations (context, userId) {
-      let query = { type: 'word-association' }
-      if (userId) query['author.id'] = userId
-      query = JSON.stringify(query)
-      const result = await axios.get(baseURL, ObjectUtil.merge(getRequestConfig(), { params: { query } }))
-      return result.data ? result.data.items : []
+      console.debug('listAssociations', userId)
+      return []
     },
     async getAssociation (context, uuid) {
-      const result = await axios.get(`${baseURL}/${uuid}`, getRequestConfig())
-      return result.data
+      console.debug('getAssociation', uuid)
     },
     async listPublicAssociations (context, userId) {
-      let query = { type: 'word-association', isPublic: true }
-      if (userId) query['author.id'] = userId
-      query = JSON.stringify(query)
-      const result = await axios.get(baseURL, ObjectUtil.merge(getRequestConfig(), { params: { query } }))
-      return result.data ? result.data.items : []
+      console.debug('listPublicAssociations', userId)
+      return []
     },
     async removeAssociation (context, uuid) {
-      const result = await axios.delete(`${baseURL}/${uuid}`, getRequestConfig())
-      return result.data
+      console.debug('removeAssociation', uuid)
     },
     async updateAssociationPublic (context, [uuid, isPublic]) {
-      const result = await axios.patch(`${baseURL}/${uuid}`, { isPublic }, getRequestConfig())
-      return result.data
+      console.debug('updateAssociationPublic', uuid, isPublic)
     }
   }
 }
