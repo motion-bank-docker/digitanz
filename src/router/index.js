8862ab43018d718f90edc965d8cd5469c3008ad1
Anton <antonymousfeedback@gmail.com>
2019-07-10 13:10:12 +0200

    Comment out auth0

--- src/router/index.js
+++ src/router/index.js
@@ -21,51 +21,51 @@ const Router = new VueRouter({
   routes
 })
 
-Router.beforeEach((to, from, next) => {
-  const message = {
-    to: to ? to.fullPath : undefined,
-    from: from ? from.fullPath : undefined
-  }
-  const waitForStore = (app, cb) => {
-    if (app.$store === undefined) setTimeout(() => waitForStore(app, cb), 0)
-    else cb()
-  }
-  waitForStore(Router.app, () => {
-    if (!Router.app.$store.state.user) {
-      Router.app.$auth.checkSession(Router.app.$store).catch(() => {
-        if (to.meta.private) {
-          // console.log('store redirect', to.fullPath)
-          // Router.app.$store.commit('auth/setRedirect', to.fullPath)
-          Router.app.$auth.authenticate()
-        }
-      }).then(result => {
-        if (result) {
-          const { user } = result
-          /*
-          if (first) {
-            console.debug('Auth0 first login', user)
-            next({ name: 'users.manage', params: { isFirst: true, redirect: to } })
-          }
-          else next()
-          */
-          next()
-          message.user = user.uuid
-        }
-        else if (to.meta.private) {
-          // console.log('store redirect', to.fullPath)
-          // Router.app.$store.commit('auth/setRedirect', to.fullPath)
-          Router.app.$auth.authenticate()
-        }
-        else next()
-        return Router.app.$store.dispatch('logging/log', { action: 'nav', message })
-      })
-    }
-    else {
-      next()
-      message.user = Router.app.$store.state.user.uuid
-      return Router.app.$store.dispatch('logging/log', { action: 'nav', message })
-    }
-  })
-})
+// Router.beforeEach((to, from, next) => {
+//   const message = {
+//     to: to ? to.fullPath : undefined,
+//     from: from ? from.fullPath : undefined
+//   }
+//   const waitForStore = (app, cb) => {
+//     if (app.$store === undefined) setTimeout(() => waitForStore(app, cb), 0)
+//     else cb()
+//   }
+//   waitForStore(Router.app, () => {
+//     if (!Router.app.$store.state.user) {
+//       Router.app.$auth.checkSession(Router.app.$store).catch(() => {
+//         if (to.meta.private) {
+//           // console.log('store redirect', to.fullPath)
+//           // Router.app.$store.commit('auth/setRedirect', to.fullPath)
+//           Router.app.$auth.authenticate()
+//         }
+//       }).then(result => {
+//         if (result) {
+//           const { user } = result
+//           /*
+//           if (first) {
+//             console.debug('Auth0 first login', user)
+//             next({ name: 'users.manage', params: { isFirst: true, redirect: to } })
+//           }
+//           else next()
+//           */
+//           next()
+//           message.user = user.uuid
+//         }
+//         else if (to.meta.private) {
+//           // console.log('store redirect', to.fullPath)
+//           // Router.app.$store.commit('auth/setRedirect', to.fullPath)
+//           Router.app.$auth.authenticate()
+//         }
+//         else next()
+//         return Router.app.$store.dispatch('logging/log', { action: 'nav', message })
+//       })
+//     }
+//     else {
+//       next()
+//       message.user = Router.app.$store.state.user.uuid
+//       return Router.app.$store.dispatch('logging/log', { action: 'nav', message })
+//     }
+//   })
+// })
 
 export default Router
