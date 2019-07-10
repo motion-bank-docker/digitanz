cfc3af5a5ad92f9cd9ebc870b17316a4e1a0ad85
Anton <antonymousfeedback@gmail.com>
2019-07-10 13:23:29 +0200

    Disable API calls

--- src/store/modules/make-resource-module.js
+++ src/store/modules/make-resource-module.js
@@ -1,32 +1,20 @@
 // const sift = require('sift')
 import { Assert } from 'mbjs-utils'
 
-const makeResourceModule = function (client, resourceName, resourceNamePluralised = undefined, overrideHost = undefined) {
+const makeResourceModule = function (client, resourceName, resourceNamePluralised = undefined) {
   const
     name = resourceName,
     namePlural = resourceNamePluralised || `${name}s`,
     idList = `${resourceName}IDs`
 
   const makeResourceAction = (action) => {
-    return (context, args) => {
+    return (context/*, args */) => {
       context.commit('setPending', action)
       /**
        * Execute action
        */
-      if (!Array.isArray(args)) args = [args]
-      return client[action](namePlural, args[0],
-        args.length > 1 ? args[1] : overrideHost, args.length > 1 ? overrideHost : undefined).then(response => {
-          if (response) {
-            if (args.length > 1) context.commit(action, [response.uuid, response])
-            else context.commit(action, response)
-            context.commit('setPending', action, false)
-            return response
-          }
-          else throw new Error(`${action} ${name} failed: empty API response`)
-        }).catch(err => {
-          context.commit('setPending', action, false)
-          throw err
-        })
+      // if (!Array.isArray(args)) args = [args]
+      if (action === 'find') return { items: [] }
     }
   }
 
