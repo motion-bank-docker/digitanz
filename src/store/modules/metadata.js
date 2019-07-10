93ef2ad0667e6a68d9d3a96cb7c39631a4901384
Anton <antonymousfeedback@gmail.com>
2019-07-10 17:46:33 +0200

    Turn metadata into stub

--- src/store/modules/metadata.js
+++ src/store/modules/metadata.js
@@ -1,29 +1,11 @@
-import axios from 'axios'
-
 const metadata = {
   namespaced: true,
   state: {},
   mutations: {},
   actions: {
     async get (context, payload) {
-      const headers = {
-        Authorization: `Bearer ${localStorage.getItem('access_token')}`
-      }
-      let metadataURL
-      if (typeof annotation === 'string') metadataURL = `${process.env.TRANSCODER_HOST}/metadata/${payload}`
-      else {
-        try {
-          metadataURL = `${process.env.TRANSCODER_HOST}/metadata/url?url=${encodeURIComponent(payload.body.source.id)}`
-        }
-        catch (e) { /* ignored */ }
-      }
-      let result
-      if (metadataURL) {
-        result = await axios.get(metadataURL, {headers})
-      }
-      const metadata = result ? result.data : {}
-      // console.debug('metadata', metadata)
-      return metadata
+      console.debug('Get metadata', payload)
+      return {}
     }
   }
 }
