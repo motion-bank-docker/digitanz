a352553875976ef8d4e0c61e74a469491b8791a4
christianrhansen <info@christianhansen.net>
2019-07-12 11:55:19 +0200

    clouds: created overview-page

---
+++ src/pages/clouds/overview.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  q-page
+    q-btn(@click="$router.push('/clouds')") create
+</template>
+
+<script>
+  export default {
+    name: 'overview'
+  }
+</script>
+
+<style scoped lang="styl">
+</style>
