99c913911a12e1178ce03c273312322839b89d14
christianrhansen <info@christianhansen.net>
2019-07-23 15:19:44 +0200

    clouds overview: selected cloud is de-selectable

--- src/pages/clouds/overview.vue
+++ src/pages/clouds/overview.vue
@@ -6,8 +6,10 @@
         template(v-for="(cloud, index) in tempClouds")
           .relative-position.overflow-hidden(style="width: 46%;")
             q-list.q-pa-none.q-mb-md.min-height-auto(no-border)
-              q-radio.full-width.q-mb-none.word-break(v-model="optionCloud", :val="index")
-                .shadow-1.round-borders.full-width(:class="[optionCloud === index ? 'bg-white text-grey-8' : 'bg-grey-4']")
+              q-radio.full-width.q-mb-none.word-break(v-model="optionCloud",
+              :val="index")
+                .shadow-1.round-borders.full-width(@click="handlerRadiobutton(index)",
+                :class="[optionCloud === index ? 'bg-white text-grey-8' : 'bg-grey-4']")
                   template(v-for="(term, i) in cloud")
                     q-item.q-pa-none.q-px-sm
                       // q-item-side.text-grey-4 {{ i }}
@@ -39,6 +41,10 @@
       })
     },
     methods: {
+      handlerRadiobutton (index) {
+        console.log(this.optionCloud, index)
+        if (index === this.optionCloud) this.optionCloud = undefined
+      },
       removeTempCloud (index) {
         this.$store.commit('cloud/removeFromTempClouds', index)
       }
