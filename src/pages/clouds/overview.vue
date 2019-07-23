6047ec5f22f5c2b8ba5c34f74e3e8533869221aa
christianrhansen <info@christianhansen.net>
2019-07-23 13:06:17 +0200

    clouds overview: cloud styling: 2 columns

--- src/pages/clouds/overview.vue
+++ src/pages/clouds/overview.vue
@@ -2,16 +2,18 @@
   q-page.q-pa-md
     .text-center.border-bottom.border-color-grey-4.q-pb-md.q-mb-md(v-if="tempClouds.length <= 0") Leer
     template(v-else)
-      .row
-        .col-8.offset-2.shadow-1.round-borders(v-for="(cloud, index) in tempClouds",
-        :class="[index < tempClouds.length - 1 ? 'q-mb-md bg-grey-4' : 'bg-white']")
-
-          q-list.q-pa-none(no-border)
-            template(v-for="(term, i) in cloud")
-              q-item.q-pa-none.q-px-sm
-                | {{ term }}
-              q-item-separator(v-if="i < cloud.length -1 && index < tempClouds.length - 1").bg-grey-5.q-ma-none
-              q-item-separator(v-if="i < cloud.length -1 && index >= tempClouds.length - 1").bg-grey-3.q-ma-none
+      .row.justify-between
+        template(v-for="(cloud, index) in tempClouds")
+          q-list.q-pa-none.q-mb-md.min-height-auto(no-border,
+          style="width: 46%;")
+            .shadow-1.round-borders(:class="[index < tempClouds.length - 1 ? 'bg-grey-4' : 'bg-white text-grey-8']")
+              template(v-for="(term, i) in cloud")
+                q-item.q-pa-none.q-px-sm
+                  // q-item-side.text-grey-4 {{ i }}
+                  q-item-main.q-caption
+                    | {{ term }}
+                q-item-separator(v-if="i < cloud.length -1 && index < tempClouds.length - 1").bg-grey-5.q-ma-none
+                q-item-separator(v-if="i < cloud.length -1 && index >= tempClouds.length - 1").bg-grey-3.q-ma-none
     .text-right
       q-btn.bg-grey-9.text-grey-2(@click="$router.push('/clouds')", no-caps, flat, icon="add", round)
 </template>
