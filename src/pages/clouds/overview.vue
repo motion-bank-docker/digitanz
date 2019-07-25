b8eae38537debc41f0b4b6c8cf1c18b9cb31b1dc
christianrhansen <info@christianhansen.net>
2019-07-25 09:27:18 +0200

    clouds overview: "empty"-message styling

--- src/pages/clouds/overview.vue
+++ src/pages/clouds/overview.vue
@@ -1,8 +1,15 @@
 <template lang="pug">
   q-page.q-pa-md
-    .text-center.border-bottom.border-color-grey-4.q-pb-md.q-mb-md(v-if="tempClouds.length <= 0") Leer
+
+    //----- "empty"-message
+    .inactive.q-mb-md
+      q-btn.full-width.capitalize.text-grey-9.shadow-3(v-if="tempClouds.length <= 0",
+      disabled, flat) Leer
+
+    //----- clouds
     template(v-else)
       .row.justify-between
+
         template(v-for="(cloud, index) in tempClouds")
           .relative-position.overflow-hidden(style="width: 46%;")
             q-list.q-pa-none.q-mb-md.min-height-auto(no-border)
@@ -12,15 +19,15 @@
                 :class="[optionCloud === index ? 'bg-white text-grey-8' : 'bg-grey-4']")
                   template(v-for="(term, i) in cloud")
                     q-item.q-pa-none.q-px-sm
-                      // q-item-side.text-grey-4 {{ i }}
-                      q-item-main.q-caption
-                        | {{ term }}
+                      q-item-main.q-caption {{ term }}
+
                     q-item-separator.q-ma-none(:class="[optionCloud === index ? 'bg-grey-3' : 'bg-grey-5']")
 
               //----- "remove"-button
               .absolute-top-right.transition.q-px-sm.q-pt-xs(:class="[optionCloud !== index ? 'leave-right-absolute' : '']")
                 q-btn.bg-grey-9.text-grey-2(icon="delete", @click="removeTempCloud(index)", round, size="sm", flat)
 
+    //----- "add"-button
     .text-right
       q-btn.bg-grey-9.text-grey-2(@click="$router.push('/clouds')", no-caps, flat, icon="add", round)
 </template>
