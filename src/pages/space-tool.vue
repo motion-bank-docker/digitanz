a6d7c02cc90017de43922467bdde2f8842da1d9e
christianrhansen <info@christianhansen.net>
2019-07-26 18:29:52 +0200

    space-tool: cleaned html. removed unused js & css.

--- src/pages/space-tool.vue
+++ src/pages/space-tool.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   q-page.bg-grey-3(style="margin-top: -1px;")
-    div(style="overflow: hidden; height: calc(100vh - 52px);")
-      lost-in-space#lostinspace
+    .overflow-hidden(style="height: calc(100vh - 52px);")
+      lost-in-space
 </template>
 
 <script>
@@ -9,14 +9,9 @@
   export default {
     components: {
       LostInSpace
-    },
-    mounted () {
-      this.window.scrollTo(52, 0)
     }
   }
 </script>
 
 <style lang="stylus" scoped>
-  #lostinspace
-    background white
 </style>
