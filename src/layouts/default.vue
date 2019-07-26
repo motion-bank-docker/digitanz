084c8031911597a07e32643955bf0684434e5ac0
christianrhansen <info@christianhansen.net>
2019-07-26 18:05:03 +0200

    default: removed commented out html & js

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -44,28 +44,10 @@
             .ellipsis(style="margin-left: -16px;") {{ currentAppName }}
 
         // info-button
+        // todo: embed info drawer?
         //
           q-item-side.q-pr-md(v-if="usingTool")
             q-btn.border.text-white(@click="handlerToolDescription", round, flat, size="sm") ?
-
-    // ---------------------------------------------------------------------------------------------------------- footer
-    //
-      q-layout-footer.text-center.bg-dark.q-py-xs(v-if="userState && !$route.meta.hideFooter")
-
-        template(v-if="hideBackButton")
-          q-btn.q-ml-md.q-mr-md(
-          v-for="button in buttons", round, flat, size="lg",
-          // :class="{ 'text-white bg-grey-9': currentApp === button.action }",
-          @click="executeApp(button.action)")
-            q-icon(:name="button.icon")
-
-          span.q-py-md
-            file-uploader-micro(:query="uploaderQuery")
-
-        template(v-else)
-          q-btn.q-mx-md(round, flat, size="lg",
-          @click="$router.go(-1)")
-            q-icon(name="arrow_back_ios")
 
     // ---------------------------------------------------------------------------------------------------------- others
     q-page-container
@@ -92,36 +74,11 @@
     data () {
       return {
         showToolDescription: false,
-        /*
-        buttons: [{
-          action: 'profile',
-          icon: 'person',
-          status: true
-        }, {
-          action: 'dashboard-new',
-          icon: 'people',
-          status: true
-        }, {
-          action: 'tools',
-          icon: 'build',
-          status: true
-        }],
-        */
-        // rootPaths: ['', 'profile', 'dashboard-new', 'tools'],
-        // hideBackButton: true,
-        // currentApp: null,
         currentAppName: ''
-        // env: process.env,
-        /*
-        uploaderQuery: {
-          'title': 'Meine Videos'
-        }
-        */
       }
     },
     computed: {
       ...mapGetters({
-        // userState: 'auth/getUserState'
       }),
       usingTool () {
         if (this.$route.path === '/' || this.$route.path === '/tools') return false
@@ -149,50 +106,16 @@
           this.currentAppName = 'Inspiration'
           break
         }
-        // this.hideBackButton = this.rootPaths.indexOf(to.fullPath.substr(1)) > -1
       }
     },
     methods: {
       handlerToolDescription () {
         this.showToolDescription = !this.showToolDescription
       },
-      /*
-      actions (val) {
-        switch (val) {
-        case 'profile':
-          this.executeApp('profile')
-          break
-        case 'dashboard-new':
-          this.executeApp('dashboard-new')
-          break
-        case 'tools':
-          this.executeApp('tools')
-          break
-        case 'unset':
-          console.log('unset')
-          break
-        }
-      },
-      */
-      /*
-      clickTitle () {
-        this.$router.push('/')
-      },
-      */
       executeApp (appName) {
         // this.currentApp = appName
         this.$router.push({path: '/' + appName})
       }
-      /*
-      login () {
-        // this.$auth.authenticate()
-      },
-      async logout () {
-        // await this.$store.dispatch('logging/log', { action: 'logout', message: this.userState.uuid })
-        // this.$store.commit('auth/setUser', undefined)
-        // this.$auth.logout()
-      }
-      */
     }
   }
 </script>
