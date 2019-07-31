f9192f5ad6951ba242f0e5d5dbcef9346e1cba1b
christianrhansen <info@christianhansen.net>
2019-07-31 17:37:15 +0200

    clouds overview: small layout fixes. replaced close button with click on zoom black.

--- src/pages/clouds/overview.vue
+++ src/pages/clouds/overview.vue
@@ -4,18 +4,22 @@
     //----- cloud in detail
     .absolute.fit.bg-grey-3.transition.overflow-hidden.transition(style="z-index: 10;",
     :class="[zoom ? '' : 'leave-right-100']")
-      .column.text-center.bg-grey-3.items-center.row(style="height: calc(100vh - 52px);")
+      .column.text-center.bg-grey-3.items-center.row(@click="handlerZoom(selectedCloud.terms, selectedCloud.index)",
+      style="height: calc(100vh - 52px);")
         .col.q-title.row(v-for="(term, index) in selectedCloud.terms")
           .self-center {{ term }}
 
       .absolute-top-right.q-ma-md
-        q-btn.bg-grey-9.text-grey-1.q-mr-sm(icon="delete", @click="removeTempCloud(selectedCloud.index)", round, size="sm", flat)
-        q-btn.bg-grey-9.text-grey-1(@click="handlerZoom", icon="clear", round, flat, size="sm")
+        //
+          div.q-bt-sm
+            q-btn.bg-grey-9.text-grey-1(@click="handlerZoom", icon="clear", round, flat, size="sm")
+        div
+          q-btn.bg-grey-9.text-grey-1(icon="delete", @click="removeTempCloud(selectedCloud.index)", round, size="sm", flat)
 
     //----- clouds
     .q-pt-sm
       template(v-if="tempClouds.length > 0")
-        q-list.min-height-auto.q-pa-sm(v-for="(cloud, index) in tempClouds", no-border)
+        q-list.min-height-auto.q-pa-sm.q-px-md(v-for="(cloud, index) in tempClouds", no-border)
           .round-borders.full-width.shadow-1.q-pa-md.text-center(@click="handlerZoom(cloud, index)")
             template(v-for="(term, i) in cloud")
               span {{ term }}
@@ -56,6 +60,7 @@
       handlerZoom (cloud, index) {
         this.selectedCloud.terms = cloud
         this.selectedCloud.index = index
+        console.log('----->', cloud)
         this.zoom = !this.zoom
       },
       removeTempCloud (index) {
