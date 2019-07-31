2015f81368d3f56a7744375e246ad9472055055d
christianrhansen <info@christianhansen.net>
2019-07-31 18:05:38 +0200

    index: hid "go"-button, click bg to start. bright bg-color.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,13 @@
 <template lang="pug">
-  q-page.bg-grey-9
+  q-page.bg-grey-3
 
-    div
+    div(@click="$router.push({path: '/tools'})")
       griddle-moves(:enclosed="true", :time="1000")
 
-    q-page-sticky.q-pb-md(position="bottom")
-      q-btn.bg-grey-2.text-grey-7.q-px-lg(@click="$router.push({path: '/tools'})",
-      label="Go!", rounded, flat)
+    //
+      q-page-sticky.q-pb-md(position="bottom")
+        q-btn.bg-grey-2.text-grey-7.q-px-lg(@click="$router.push({path: '/tools'})",
+        label="Go!", rounded, flat)
 
 </template>
 
