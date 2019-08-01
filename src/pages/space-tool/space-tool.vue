a77cf046250eb4c54324c1b77f01b38ca27d581b
christianrhansen <info@christianhansen.net>
2019-08-01 17:16:41 +0200

    moved space-tool in an own directory

--- src/pages/space-tool/space-tool.vue
+++ src/pages/space-tool/space-tool.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-  import LostInSpace from '../components/LostInSpace'
+  import LostInSpace from '../../components/LostInSpace'
   export default {
     components: {
       LostInSpace
