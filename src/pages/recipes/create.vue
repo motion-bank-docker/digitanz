11bfc138e49815abc96ff5811e688f5381fdc028
christianrhansen <info@christianhansen.net>
2019-08-05 12:15:17 +0200

    recipes create: increased icon-sizes in input-box

--- src/pages/recipes/create.vue
+++ src/pages/recipes/create.vue
@@ -44,9 +44,12 @@
             .absolute-top-right.transition.q-px-sm.items-start.row.q-mt-sm.q-pt-xs(
             :class="[option !== ingr ? 'leave-right-absolute' : '']",
             style="margin-top: 10px;")
-              q-btn.bg-grey-3.text-grey-9.shadow-1(icon="keyboard_arrow_up", @click="moveUp(index)", round, size="sm", flat)
-              q-btn.bg-grey-3.text-grey-9.shadow-1.q-ml-sm.q-mr-md(icon="keyboard_arrow_down", @click="moveDown(index)", round, size="sm", flat)
-              q-btn.bg-grey-3.text-grey-9.shadow-1(@click="deleteTodoItem(index)", icon="delete", round, size="sm", flat)
+              q-btn.bg-grey-3.text-grey-9.shadow-1(@click="moveUp(index)", round, size="sm", flat)
+                q-icon(name="keyboard_arrow_up", size="18px")
+              q-btn.bg-grey-3.text-grey-9.shadow-1.q-ml-sm.q-mr-md(@click="moveDown(index)", round, size="sm", flat)
+                q-icon(name="keyboard_arrow_down", size="18px")
+              q-btn.bg-grey-3.text-grey-9.shadow-1(@click="deleteTodoItem(index)", round, size="sm", flat)
+                q-icon(name="delete", size="16px")
 
         q-item-separator.q-ma-none.bg-grey-5.opacity-4(v-if="newRecipe.entries.length")
 
@@ -62,8 +65,10 @@
             :after="[{icon: 'edit', condition: showIcon.newRecipeEntry && !addIngredient}]")
           q-item-side.min-width-auto.min-height-auto.transition.row.self-stretch(:class="[!addIngredient ? 'leave-right' : '']")
             .transition.q-px-sm.items-top.row.q-pt-xs.q-mt-sm
-              q-btn.bg-grey-3.text-grey-9.shadow-1(@click="addTodoItem", icon="add", round, size="sm", flat)
-              q-btn.bg-grey-3.text-grey-9.shadow-1.q-ml-sm(@click="resetValues", round, icon="clear", size="sm", flat)
+              q-btn.bg-grey-3.text-grey-9.shadow-1(@click="addTodoItem", round, size="sm", flat)
+                q-icon(name="add", size="16px")
+              q-btn.bg-grey-3.text-grey-9.shadow-1.q-ml-sm(@click="resetValues", round, size="sm", flat)
+                q-icon(name="clear", size="16px")
 
     // ----------------------------------------------------------------------------------------------- buttons at bottom
     .q-mx-md.q-mt-md.overflow-hidden
