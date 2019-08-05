eb15b94d9bde0ca13ad4d8857a583d340cfc0497
christianrhansen <info@christianhansen.net>
2019-08-05 17:17:14 +0200

    space-tool store: added existing shapes to store

--- src/store/modules/space-tool.js
+++ src/store/modules/space-tool.js
@@ -29,6 +29,37 @@ const spaceTool = {
       elements: [
         {type: 'polygon', points: '0,0 100,0 100,100'}
       ]
+    }, {
+      id: '#rect-1',
+      elements: [
+        {type: 'rect', x: '0', y: '0', width: '100', height: '100'}
+      ]
+    }, {
+      id: '#ellipses-1',
+      elements: [
+        {type: 'ellipse', cx: '50', cy: '50', rx: '50', ry: '50'}
+      ]
+    }, {
+      id: '#ellipses-2',
+      elements: [
+        {type: 'ellipse', cx: '50', cy: '50', rx: '40', ry: '40'}
+      ]
+    }, {
+      id: '#ellipses-3',
+      elements: [
+        {type: 'ellipse', cx: '50', cy: '50', rx: '25', ry: '25'}
+      ]
+    }, {
+      id: '#ellipses-4',
+      elements: [
+        {type: 'ellipse', cx: '30', cy: '30', rx: '12', ry: '12'},
+        {type: 'ellipse', cx: '60', cy: '60', rx: '25', ry: '25'}
+      ]
+    }, {
+      id: '#traingle-4',
+      elements: [
+        {type: 'polygon', points: '53,100 3,25 93,25'}
+      ]
     }]
   },
   getters: {
