39a81c80cc9cebd5fdfa73110632f606e3d47642
christianrhansen <info@christianhansen.net>
2019-08-06 17:45:36 +0200

    space-tool list: removed unused html & js

--- src/pages/space-tool/list.vue
+++ src/pages/space-tool/list.vue
@@ -7,12 +7,9 @@
     .row.q-px-sm.q-mb-md.q-pb-md
       .col-3.q-px-sm.q-mt-md.text-center.round-borders.items-center(v-for="shape in shapes")
 
-        input.hidden(v-model="checkboxSelectedShapes", type="checkbox", :id="shape.id", :value="shape.id")
-
-        label.full-width.full-height.self-center(:for="shape.id")
-          div.shadow-1.q-px-sm.bg-e4.text-grey-8.round-borders(
-          @click="addToSelection(shape)")
-            shape-icon(:shape="shape", :cols="4")
+        div.shadow-1.q-px-sm.bg-e4.text-grey-8.round-borders(
+        @click="addToSelection(shape)")
+          shape-icon(:shape="shape", :cols="4")
 
     .fixed-bottom-right.q-px-md.q-mt-md.q-mb-md
       q-btn(@click="$router.push('/space-tool')", round, flat,
@@ -30,7 +27,6 @@
   export default {
     data () {
       return {
-        checkboxSelectedShapes: [],
         selection: []
       }
     },
@@ -47,9 +43,6 @@
       if (this.selectedShapes.length > 0) this.selection = this.selectedShapes
     },
     watch: {
-      // checkboxSelectedShapes (val) {
-      //   this.$store.commit('spaceTool/setSelectedShapes', val)
-      // }
     },
     methods: {
       checkIfSelected (val) {
