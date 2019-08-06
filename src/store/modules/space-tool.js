c90c931d1cf45dab41de821e804a0e02fe54b039
christianrhansen <info@christianhansen.net>
2019-08-06 11:45:16 +0200

    space-tool store: shape-ids fixed

--- src/store/modules/space-tool.js
+++ src/store/modules/space-tool.js
@@ -2,12 +2,12 @@ const spaceTool = {
   namespaced: true,
   state: {
     shapes: [{
-      id: 'diagonal-lines-1',
+      id: '#diagonal-lines-1',
       elements: [
         {type: 'line', x1: '0', y1: '0', x2: '100', y2: '100'}
       ]
     }, {
-      id: 'diagonal-lines-2',
+      id: '#diagonal-lines-2',
       elements: [
         {type: 'line', x1: '0', y1: '0', x2: '100', y2: '100'},
         {type: 'line', x1: '100', y1: '0', x2: '0', y2: '100'}
