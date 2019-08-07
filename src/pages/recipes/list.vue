03916d83a602c9fd2f8c18590cee065af73bc3ca
christianrhansen <info@christianhansen.net>
2019-08-07 11:09:06 +0200

    recipes list: meine rezepte: empty list layout

--- src/pages/recipes/list.vue
+++ src/pages/recipes/list.vue
@@ -87,7 +87,18 @@
 
         //----- "add"-button
         template(v-if="!recipeStandalone")
-          q-item.q-pa-none.row.items-center
+          div.row.items-center.height-empty(v-if="tempRecipes.length <= 0")
+            q-item.text-center.q-ma-none.full-width
+              q-item-main.q-mb-xl
+                q-item-tile.inactive.q-title.text-weight-light
+                  | Noch keine Einträge,
+                  br
+                  | füge einen hinzu.
+                q-item-tile.q-pt-sm.q-mt-xs
+                  q-btn.bg-grey-9.text-white(
+                  @click="$router.push('/recipes/create')", round, flat)
+                    q-icon(name="add")
+          q-item.q-pa-none.row.items-center(v-else)
             q-item-side.inactive(v-if="tempRecipes.length <= 0") Leer
 
             q-item-main.text-right
@@ -238,7 +249,10 @@
 
   .leave-right-absolute
     right -50vw
+  .height-empty
+    height calc(100vh - 78px - 52px - 16px - 8px)
 </style>
+
 <style lang="stylus">
   .q-popover#zoom-title
     max-width calc(100vw - 16px - 16px - 16px - 16px)!important
