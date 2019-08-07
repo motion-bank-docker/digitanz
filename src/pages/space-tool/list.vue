3e7e7416eec151847568f7c870adfcd679796312
christianrhansen <info@christianhansen.net>
2019-08-07 17:06:01 +0200

    space-tool list: empty screen layout

--- src/pages/space-tool/list.vue
+++ src/pages/space-tool/list.vue
@@ -1,8 +1,25 @@
 <template lang="pug">
   q-page.min-height-auto.q-pa-md
-    | create
-    div(v-for="space in spaces") {{ space }}
-    q-btn(@click="$router.push('/space-tool')") go
+
+    div(v-for="space in tempSpaces") {{ space }}
+
+    template
+      div.row.items-center.height-empty(v-if="tempSpaces.length <= 0")
+        q-item.text-center.q-ma-none.full-width
+          q-item-main.q-mb-xl
+            q-item-tile.inactive.q-title.text-weight-light
+              | Noch keine Einträge,
+              br
+              | füge einen hinzu.
+            q-item-tile.q-pt-sm.q-mt-xs
+              q-btn.bg-grey-9.text-white(
+              @click="$router.push('/space-tool/create')", round, flat)
+                q-icon(name="add")
+      q-item.q-pa-none.row.items-center(v-else)
+        q-item-main.text-right
+          q-btn.bg-grey-9.text-white(
+          @click="$router.push('/space-tool/create')", round, flat)
+            q-icon(name="add")
 </template>
 
 <script>
@@ -17,11 +34,13 @@
       ...mapGetters({
         shapes: 'spaceTool/getShapes',
         selectedShapes: 'spaceTool/getSelectedShapes',
-        spaces: 'spaceTool/getSpaces'
+        tempSpaces: 'spaceTool/getTempSpaces'
       })
     }
   }
 </script>
 
 <style lang="stylus" scoped>
+  .height-empty
+    height calc(100vh - 52px - 16px)
 </style>
