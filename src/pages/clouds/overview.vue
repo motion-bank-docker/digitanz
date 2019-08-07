e6b7a8373ed06191bac3e1521bd18b891b835c0f
christianrhansen <info@christianhansen.net>
2019-08-07 12:48:48 +0200

    clouds overview: empty screen layout

--- src/pages/clouds/overview.vue
+++ src/pages/clouds/overview.vue
@@ -43,12 +43,30 @@
 
     // ---------------------------------------------------------------------------------------------------- "add"-button
     .q-px-md.q-pb-md.q-pt-sm
-      q-item.q-pa-none.row.items-center
-        q-item-side.inactive(v-if="tempClouds.length <= 0") Leer
+      div.row.items-center.height-empty(v-if="tempClouds.length <= 0")
+        q-item.text-center.q-ma-none.full-width
+          q-item-main.q-mb-xl
+            q-item-tile.inactive.q-title.text-weight-light
+              | Noch keine Einträge,
+              br
+              | füge einen hinzu.
+            q-item-tile.q-pt-sm.q-mt-xs
+              q-btn.bg-grey-9.text-white(@click="$router.push('/clouds')", round, flat)
+                q-icon(name="add")
+      q-item.q-pa-none.row.items-center(v-else)
+        // q-item-side.inactive(v-if="tempRecipes.length <= 0") Leer
 
         q-item-main.text-right
-          q-btn.bg-grey-9.text-white(@click="$router.push('/clouds')", round, flat)
+          q-btn.bg-grey-9.text-white(
+          @click="$router.push('/recipes/create')", round, flat)
             q-icon(name="add")
+      //
+        q-item.q-pa-none.row.items-center
+          q-item-side.inactive(v-if="tempClouds.length <= 0") Leer
+
+          q-item-main.text-right
+            q-btn.bg-grey-9.text-white(@click="$router.push('/clouds')", round, flat)
+              q-icon(name="add")
 
 </template>
 
@@ -90,6 +108,9 @@
 
 <style scoped lang="styl">
   @import '~variables'
+
+  .height-empty
+    height calc(100vh - 52px - 16px - 8px)
 </style>
 
 <style lang="styl">
