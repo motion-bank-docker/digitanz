cd443ebe33cf518311f02acd82737f53b66cdd7e
christianrhansen <info@christianhansen.net>
2019-08-08 14:45:10 +0200

    space-tool: extended handlerButton-function.

--- src/pages/space-tool/list.vue
+++ src/pages/space-tool/list.vue
@@ -4,29 +4,30 @@
     // ---------------------------------------------------------------------------------------------------------- spaces
     .bg-e4.q-mt-md(v-for="(space, index) in tempSpaces")
       .relative-position.overflow-hidden.round-borders
+
         q-radio.q-mb-none(v-model="optionSpace", :val="index",
         :class="{'bg-grey-1': optionSpace === index}")
-
           .row.q-pa-md(@click="handlerRadiobutton(index)",
           :class="[space.length <= 3 ? 'justify-center' : '']")
             .col-3.q-px-md.q-py-sm.round-borders.relative-position(v-for="shape in space")
               shape-icon(:shape="shape", :cols="4")
 
-        //----- "edit"-button
-        //----- "remove"-button
-        //----- "play"-button
         .absolute-top-right.transition.q-px-sm.q-pt-sm(:class="[optionSpace !== index ? 'leave-right-absolute' : '']")
+          //----- "play"-button
           q-btn.bg-grey-3.text-grey-9.shadow-1(@click="handlerSpaceButtons('play', index)", flat, size="sm", round)
             q-icon(name="play_arrow", size="18px")
 
+          //----- "edit"-button
           q-btn.bg-grey-3.text-grey-9.shadow-1.q-mx-sm(@click="handlerSpaceButtons('edit', index)", round, size="sm", flat)
             q-icon(name="edit", size="16px")
 
+          //----- "delete"-button
           q-btn.bg-grey-3.text-grey-9.shadow-1(@click="handlerSpaceButtons('delete', index)", round, size="sm", flat)
             q-icon(name="delete", size="16px")
 
     // --------------------------------------------------------------------------------------------------- buttons below
     template
+
       div.row.items-center.height-empty(v-if="tempSpaces.length <= 0")
         q-item.text-center.q-ma-none.full-width
           q-item-main.q-mb-xl
@@ -36,12 +37,13 @@
               | füge einen hinzu.
             q-item-tile.q-pt-sm.q-mt-xs
               q-btn.bg-grey-9.text-white(
-              @click="$router.push('/space-tool/create')", round, flat)
+              @click="handlerSpaceButtons('add')", round, flat)
                 q-icon(name="add")
+
       q-item.q-pa-none.row.items-center.q-mt-md(v-else)
         q-item-main.text-right
           q-btn.bg-grey-9.text-white(
-          @click="$router.push('/space-tool/create')", round, flat)
+          @click="handlerSpaceButtons('add')", round, flat)
             q-icon(name="add")
 </template>
 
@@ -74,9 +76,12 @@
         console.log(space)
         this.$router.push('/space-tool/create')
       },
-      handlerSpaceButtons (action, space) {
-        console.log('handlerSpaceButtons - space', space)
+      handlerSpaceButtons (action, index) {
+        console.log('handlerSpaceButtons - index', index)
         switch (action) {
+        case 'add':
+          this.$router.push('/space-tool/create')
+          break
         case 'edit':
           this.$router.push('/space-tool/create')
           break
