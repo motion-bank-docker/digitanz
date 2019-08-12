371f6e35edc1c2eef88eaee448aa243cd1797999
christianrhansen <info@christianhansen.net>
2019-08-12 17:09:26 +0200

    InfoButton: fixed sizes

--- src/components/InfoButton.vue
+++ src/components/InfoButton.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
-  q-btn(flat, :size="spinnerSize", round)
-    q-spinner-puff(color="grey-9", size="30px")
+  q-btn(flat, :size="buttonSize", round)
+    q-spinner-puff(color="grey-9", :size="spinnerSize")
     q-popover.bg-grey-9.q-px-md.q-py-sm.text-grey-1.q-caption
       slot
 </template>
@@ -10,14 +10,24 @@
     name: 'info-button',
     data () {
       return {
-        spinnerSize: undefined
+        spinnerSize: undefined,
+        buttonSize: undefined
       }
     },
     props: ['size'],
     mounted () {
-      if (this.size === ('md' || '')) this.spinnerSize = 30
-      if (this.size === 'sm') this.spinnerSize = 20
-      if (this.size === 'lg') this.spinnerSize = 40
+      if (this.size === ('md' || '')) {
+        this.spinnerSize = '30'
+        this.buttonSize = 'md'
+      }
+      if (this.size === 'sm') {
+        this.spinnerSize = '20'
+        this.buttonSize = 'sm'
+      }
+      if (this.size === 'lg') {
+        this.spinnerSize = '40'
+        this.buttonSize = 'lg'
+      }
     }
   }
 </script>
