665d4aa2b2b5680588719efb1d6c859198b3a615
christianrhansen <info@christianhansen.net>
2019-08-13 13:23:57 +0200

    created InfoButton-component for usage in InfoButton-components popover

---
+++ src/components/InfoNumber.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  span.relative-position.text-center.q-px-xs(style="width: 2em;")
+    | &nbsp;
+    | {{ number }}
+    | &nbsp;
+    .absolute-top-left.full-width.full-height(style="width: 2em; margin-top: -5px;")
+      q-spinner-puff(color="grey-1", :size="24")
+</template>
+
+<script>
+  export default {
+    name: 'info-number',
+    props: ['number']
+  }
+</script>
+
+<style scoped>
+</style>
