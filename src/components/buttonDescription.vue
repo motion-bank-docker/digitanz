5a5aa49c05bbaa8d16e7b1e718cfb398bf7e67cf
christianrhansen <info@christianhansen.net>
2019-08-13 19:11:25 +0200

    buttonDescription: variable fix

--- src/components/buttonDescription.vue
+++ src/components/buttonDescription.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   .row.full-width
     .col-1.text-center
-      q-icon(:name="iconName", :size="iconSize")
+      q-icon(:name="iName", :size="iSize")
     .col-11.q-pl-sm
       slot
 </template>
@@ -10,8 +10,16 @@
   export default {
     name: 'button-description',
     props: ['iconName', 'iconSize'],
+    data () {
+      return {
+        iName: undefined,
+        iSize: undefined
+      }
+    },
     mounted () {
-      if (this.iconSize === undefined) this.iconSize = '14px'
+      if (this.iconSize === undefined) this.iSize = '14px'
+      this.iName = this.iconName
+      this.iSize = this.iconSize
     }
   }
 </script>
