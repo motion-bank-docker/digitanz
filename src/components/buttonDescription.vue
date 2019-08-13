72d38d8d5094a831d2ef8c475bb122f26cc25d01
christianrhansen <info@christianhansen.net>
2019-08-13 19:07:59 +0200

    created buttonDescription for info-button popover, describing buttons

---
+++ src/components/buttonDescription.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  .row.full-width
+    .col-1.text-center
+      q-icon(:name="iconName", :size="iconSize")
+    .col-11.q-pl-sm
+      slot
+</template>
+
+<script>
+  export default {
+    name: 'button-description',
+    props: ['iconName', 'iconSize'],
+    mounted () {
+      if (this.iconSize === undefined) this.iconSize = '14px'
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
