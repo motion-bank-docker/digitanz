578e15e2ead96f780e5e052d1ec070975031b557
christianrhansen <info@christianhansen.net>
2019-08-13 19:36:36 +0200

    space-tool: added info-buttons

--- src/pages/space-tool/space-tool.vue
+++ src/pages/space-tool/space-tool.vue
@@ -3,8 +3,20 @@
     .overflow-hidden(style="height: calc(100vh - 52px);")
       lost-in-space
 
+    .absolute-top-left.q-mt-md.q-ml-md
+
+      info-button.q-mt-xs(:size="'sm'")
+        | Tippe auf den Screen um zur nächsten Form zu gelangen.
+
     .absolute-top-right.q-ma-md
-      q-btn.text-grey-9.bg-grey-3.shadow-1.q-mr-sm(@click="$router.push('/space-tool/create')", round, flat, size="sm")
+
+      info-button.q-mt-xs(:size="'sm'")
+        button-description.q-mb-sm(:iconName="'edit'")
+          | Bearbeite deine Auswahl.
+        button-description(:iconName="'clear'")
+          | Zurück zur Übersicht aller erstellten Spaces.
+
+      q-btn.text-grey-9.bg-grey-3.shadow-1.q-mx-sm(@click="$router.push('/space-tool/create')", round, flat, size="sm")
         q-icon(name="edit", size="16px")
       q-btn.text-grey-9.bg-grey-3.shadow-1(@click="$router.push('/space-tool/list')", round, flat, size="sm")
         q-icon(name="clear", size="16px")
@@ -12,9 +24,14 @@
 
 <script>
   import LostInSpace from '../../components/LostInSpace'
+  import infoButton from '../../components/InfoButton'
+  import buttonDescription from '../../components/ButtonDescription'
+
   export default {
     components: {
-      LostInSpace
+      LostInSpace,
+      infoButton,
+      buttonDescription
     }
   }
 </script>
