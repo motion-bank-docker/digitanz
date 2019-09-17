5660083b3a4e966a462beb9bdbc80ba180207edc
christianrhansen <info@christianhansen.net>
2019-09-17 15:51:55 +0200

    fixed broken resizing when reaching minimum amount of rows/columns

--- src/store/modules/mr-griddle.js
+++ src/store/modules/mr-griddle.js
@@ -32,10 +32,10 @@ const poses = {
       if (param) state.tempGrid.columns = param
     },
     setTempGrid (state, val) {
-      if (state.tempGrid.rows > 2) state.tempGrid.rows += (val.rows * 2)
-      else if (state.tempGrid.rows === 2 && val.rows === 1) state.tempGrid.rows += (val.rows * 2)
-      if (state.tempGrid.columns > 2) state.tempGrid.columns += (val.columns * 2)
-      else if (state.tempGrid.columns === 2 && val.columns === 1) state.tempGrid.columns += (val.columns * 2)
+      if (state.tempGrid.rows > 4) state.tempGrid.rows += (val.rows * 2)
+      else if (state.tempGrid.rows === 4 && Math.sign(val.rows) > 0) state.tempGrid.rows += (val.rows * 2)
+      if (state.tempGrid.columns > 4) state.tempGrid.columns += (val.columns * 2)
+      else if (state.tempGrid.columns === 4 && Math.sign(val.columns) > 0) state.tempGrid.columns += (val.columns * 2)
     },
     setTempFrameLength (state, val) {
       state.frameLength = val
