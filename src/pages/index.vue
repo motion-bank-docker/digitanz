a3254ce86b4dddb375988fde761dbf22c5c9763d
christianrhansen <info@christianhansen.net>
2019-09-25 18:55:08 +0200

    added imprint content

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -39,16 +39,136 @@
 
         // ------------------------------------------------------------------------------------------------- description
         // div.bg-grey-3.text-grey-9.full-width.q-px-md.q-pb-md
-        .row.col-xs-12.col-sm-8.offset-sm-2.col-md-6.offset-md-3.text-grey-9.q-pb-md
-          .col-6.q-px-md
-            | Holzstraße 36
-            br
-            | 55116 Mainz
-          .col-6.q-px-md
-            | Mail
-            br
-            | bla
+        .row.col-xs-12.col-sm-8.offset-sm-2.col-md-6.offset-md-3.text-grey-9.q-pb-md.q-px-md.hyphens
+          | Impressum
+          div
+            p digitanz lite (lite.digitanz.de)
+            p Hochschule Mainz
+            p Fachrichtung Kommunikationsdesign / Projekt Motion Bank
+            p Holzstrasse 36, 55116 Mainz
+            p Telefon: +49 (0) 61 31 - 628-2211
+            p E-Mail: florian.jenett (at) hs-mainz.de
+          div
+            p Gesetzlicher Vertreter
+            p Präsident der Hochschule, Prof. Dr.-Ing. Gerhard Muth
 
+          div
+            p Zuständige Aufsichtsbehörde
+            p Ministerium für Wissenschaft
+            p Weiterbildung und Kultur des Landes Rheinland-Pfalz
+            p Mittlere Bleiche 61
+            p 55116 Mainz
+
+          div
+            p Verantwortliche I. S. D. § 55 ABS. 2 RFSTV
+            p Prof. Dr.-Ing. Gerhard Muth
+
+          div
+            p Copyright
+            p Hochschule Mainz, Projekt Motion Bank sowie die Autoren von Text und Bild.
+
+          div
+            p Rechtlicher Hinweis
+            p Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschlie?lich deren Betreiber zuständig.
+
+          div
+            p Haftungsausschluss
+            p Mit Urteil vom 12. Mai 1998 hat das Landgericht Hamburg entschieden, dass man durch die Ausbringung eines Links die Inhalte der gelinkten Seite ggf. mit zu verantworten hat. Dies kann - so das LG - nur dadurch verhindert werden, dass man sich ausdr?cklich von diesen Inhalten distanziert. Der Studiengang Kommunikationsdesign der Hochschule Mainz hat auf ihrer Website Links zu anderen Seiten im Internet gelegt. F?r alle diese Links gilt: Es wird ausdr?cklich betont, dass der Studiengang Kommunikationsdesign der Hochschule Mainz keinerlei Einfluss auf die Gestaltung und die Inhalte der verlinkten Seiten hat. Deshalb distanziert sich der Studiengang Kommunikationsdesign der Hochschule Mainz hiermit ausdrücklich von allen Inhalten aller gelinkten Seiten auf dieser Homepage und macht sich ihre Inhalte nicht zueigen. Diese Erklärung gilt für alle auf dieser Homepage ausgebrachten Links. Alle genannten Warenzeichen und Marken sind Besitz ihrer jeweilig registrierten Eigent?mer.
+
+          div
+            p DATENSCHUTZ
+
+          div
+            p VERANTWORTLICHER UND DATENSCHUTZBEAUFTRAGTE
+            p Name und Kontaktdaten des für die Verarbeitung Verantwortlichen sowie der betrieblichen Datenschutzbeauftragten.
+
+          div
+            p Verantwortlicher
+            p Präsident der Hochschule Mainz
+            p Lucy-Hillebrand-Str. 2
+            p 55218 Mainz
+            p praesident (at) hs-mainz.de
+
+          div
+            p Datenschutzbeauftragte
+            p Datenschutzbeauftragte der Hochschule Mainz
+            p Lucy-Hillebrand-Str. 2
+            p 55218 Mainz
+            p datenschutz (at) hs-mainz.de
+
+          div
+            p ERHEBUNG UND SPEICHERUNG PERSONENBEZOGENER DATEN SOWIE ART UND ZWECK DER VERWENDUNG
+            p BEIM BESUCH DER WEBSITE
+            p Beim Aufrufen unserer Website www.hs-mainz.de werden durch den auf Ihrem Endgerät zum Einsatz kommenden Browser automatisch Informationen an den Server unserer Website gesendet. Diese Informationen werden temporär in einem sog. Logfile gespeichert. Folgende Informationen werden dabei ohne Ihr Zutun erfasst und bis zur automatisierten Löschung gespeichert:
+
+            ul
+              li IP-Adresse des anfragenden Rechners
+              li Datum und Uhrzeit des Zugriffs
+              li Name und URL der abgerufenen Datei
+              li Website, von der aus der Zugriff erfolgt (Referrer-URL)
+              li verwendeter Browser und ggf. das Betriebssystem Ihres Rechners sowie der Name Ihres Access-Providers
+          div
+            p Die genannten Daten werden durch uns zu folgenden Zwecken verarbeitet:
+            ul
+              li Gewährleistung eines reibungslosen Verbindungsaufbaus der Website
+              li Gewährleistung einer komfortablen Nutzung unserer Website
+              li Auswertung der Systemsicherheit und -stabilität sowie zu weiteren administrativen Zwecken.
+
+          div
+            p
+              | Die Rechtsgrundlage für die Datenverarbeitung ist Art. 6 Abs. 1 S. 1 lit. f DS-GVO. Unser berechtigtes Interesse folgt aus oben aufgelisteten Zwecken zur Datenerhebung. In keinem Fall verwenden wir die erhobenen Daten zu dem Zweck, Rückschlüsse auf Ihre Person zu ziehen.
+              br
+              | Darüber hinaus setzen wir beim Besuch unserer Website Cookies sowie Analysedienste ein. Nähere Erläuterungen dazu erhalten Sie weiter unten in dieser Datenschutzerklärung.
+
+          div
+            p BEI NUTZUNG UNSERES KONTAKTFORMULARS
+            p Bei Fragen jeglicher Art bieten wir Ihnen die Möglichkeit, mit uns über ein auf der Website bereitgestelltes Formular Kontakt aufzunehmen. Dabei ist die Angabe Ihres Namens und einer gültigen E-Mail-Adresse erforderlich, damit wir wissen, von wem die Anfrage stammt und um diese beantworten zu können.
+            p Die Datenverarbeitung zum Zwecke der Kontaktaufnahme mit uns erfolgt nach Art. 6 Abs. 1 S. 1 lit. a DS-GVO auf Grundlage Ihrer freiwillig erteilten Einwilligung.
+            p Die für die Benutzung des Kontaktformulars von uns erhobenen personenbezogenen Daten werden nach Erledigung der von Ihnen gestellten Anfrage gelöscht.
+
+          div
+            p WEITERGABE VON DATEN
+            p Eine Übermittlung Ihrer persönlichen Daten an Dritte zu anderen als den im Folgenden aufgeführten Zwecken findet nicht statt.
+            p Wir geben Ihre persönlichen Daten nur an Dritte weiter, wenn für die Weitergabe nach Art. 6 Abs. 1 S. 1 lit. c DS-GVO eine gesetzliche Verpflichtung besteht.
+
+          div
+            p COOKIES
+            p Wir setzen auf unserer Seite Cookies ein. Hierbei handelt es sich um kleine Dateien, die Ihr Browser automatisch erstellt und die auf Ihrem Endgerät (Laptop, Tablet, Smartphone o.ä.) gespeichert werden, wenn Sie unsere Seite besuchen. Cookies richten auf Ihrem Endgerät keinen Schaden an, enthalten keine Viren, Trojaner oder sonstige Schadsoftware.
+            p In dem Cookie werden Informationen abgelegt, die sich jeweils im Zusammenhang mit dem spezifisch eingesetzten Endgerät ergeben. Dies bedeutet jedoch nicht, dass wir dadurch unmittelbar Kenntnis von Ihrer Identität erhalten.
+            p Zum anderen setzten wir Cookies ein, um die Nutzung unserer Website statistisch zu erfassen und zum Zwecke der Optimierung unseres Angebotes für Sie auszuwerten. Diese Cookies ermöglichen es uns, bei einem erneuten Besuch unserer Seite automatisch zu erkennen, dass Sie bereits bei uns waren. Diese Cookies werden nach einer jeweils definierten Zeit automatisch gelöscht.
+            p Die durch Cookies verarbeiteten Daten sind für die genannten Zwecke zur Wahrung unserer berechtigten Interessen sowie der Dritter nach Art. 6 Abs. 1 S. 1 lit. f DS-GVO erforderlich.
+            p Die meisten Browser akzeptieren Cookies automatisch. Sie können Ihren Browser jedoch so konfigurieren, dass keine Cookies auf Ihrem Computer gespeichert werden oder stets ein Hinweis erscheint, bevor ein neuer Cookie angelegt wird. Die vollständige Deaktivierung von Cookies kann jedoch dazu führen, dass Sie nicht alle Funktionen unserer Website nutzen können.
+
+          div
+            p VERWEISE ZU SOCIAL MEDIA ANBIETER
+            p Die Verweise sind im Rahmen unseres Internetauftritts jeweils durch das Logo des Anbieters kenntlich gemacht. Social Plugins werden nicht eingesetzt. Bei Ihrem Besuch auf unseren Webseiten mit den Verweisen zu den sozialen Netzwerken werden also keine Daten an Dritte übermittelt. Erst wenn Sie die Webseite des Social Media Dienstes aufgerufen und sich bei diesem Dienst registriert oder angemeldet haben, erfolgt eine Übertragung der Daten.
+            p Wir tragen keine Verantwortung dafür, ob die Betreiber der Social Media Plattformen die gesetzlichen Datenschutzbestimmungen einhalten.
+
+          div
+            p BETROFFENENRECHTE
+            p Sie haben das Recht:
+            p gemäß Art. 15 DS-GVO Auskunft über Ihre von uns verarbeiteten personenbezogenen Daten zu verlangen. Insbesondere können Sie Auskunft über die Verarbeitungszwecke, die Kategorie der personenbezogenen Daten, die Kategorien von Empfängern, gegenüber denen Ihre Daten offengelegt wurden oder werden, die geplante Speicherdauer, das Bestehen eines Rechts auf Berichtigung, Löschung, Einschränkung der Verarbeitung oder Widerspruch, das Bestehen eines Beschwerderechts, die Herkunft ihrer Daten, sofern diese nicht bei uns erhoben wurden, sowie über das Bestehen einer automatisierten Entscheidungsfindung einschließlich Profiling und ggf. aussagekräftigen Informationen zu deren Einzelheiten verlangen;
+            p gemäß Art. 16 DS-GVO unverzüglich die Berichtigung unrichtiger oder Vervollständigung Ihrer bei uns gespeicherten personenbezogenen Daten zu verlangen;
+            p gemäß Art. 17 DS-GVO die Löschung Ihrer bei uns gespeicherten personenbezogenen Daten zu verlangen, soweit nicht die Verarbeitung zur Ausübung des Rechts auf freie Meinungsäußerung und Information, zur Erfüllung einer rechtlichen Verpflichtung, aus Gründen des öffentlichen Interesses oder zur Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen erforderlich ist;
+            p gemäß Art. 18 DS-GVO die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen, soweit die Richtigkeit der Daten von Ihnen bestritten wird, die Verarbeitung unrechtmäßig ist, Sie aber deren Löschung ablehnen und wir die Daten nicht mehr benötigen, Sie jedoch diese zur Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen benötigen oder Sie gemäß Art. 21 DS-GVO Widerspruch gegen die Verarbeitung eingelegt haben;
+            p gemäß Art. 20 DS-GVO Ihre personenbezogenen Daten, die Sie uns bereitgestellt haben, in einem strukturierten, gängigen und maschinenlesebaren Format zu erhalten oder die Übermittlung an einen anderen Verantwortlichen zu verlangen;
+            p gemäß Art. 7 Abs. 3 DS-GVO Ihre einmal erteilte Einwilligung jederzeit gegenüber uns zu widerrufen. Dies hat zur Folge, dass wir die Datenverarbeitung, die auf dieser Einwilligung beruhte, für die Zukunft nicht mehr fortführen dürfen und
+            p gemäß Art. 77 DS-GVO sich bei einer Aufsichtsbehörde zu beschweren. In der Regel können Sie sich hierfür an die Aufsichtsbehörde Ihres üblichen Aufenthaltsortes oder Arbeitsplatzes oder unseres Vereinssitzes wenden.
+            p Sie haben das Recht, Ihre datenschutzrechtliche Einwilligungserklärung jederzeit zu widerrufen. Durch den Widerruf der Einwilligung wird die Rechtmäßigkeit der aufgrund der Einwilligung bis zum Widerruf erfolgten Verarbeitung nicht berührt.
+
+          div
+            p RECHT AUF BESCHWERDE BEI EINER AUFSICHTSBEHÖRDE
+            p Unbeschadet eines anderweitigen verwaltungsrechtlichen oder gerichtlichen Rechtsbehelfs steht Ihnen das Recht auf Beschwerde bei einer Aufsichtsbehörde, insbesondere in dem Mitgliedstaat ihres Aufenthaltsorts, ihres Arbeitsplatzes oder des Orts des mutmaßlichen Verstoßes, zu, wenn Sie der Ansicht sind, dass die Verarbeitung der Sie betreffenden personenbezogenen Daten gegen die DS-GVO verstößt.
+            p Das Beschwerderecht gemäß Artikel 13 DS-GVO ist bei der für die Hochschule Mainz zuständigen Aufsichtsbehörde geltend zu machen:
+
+          div
+            | Der Landesbeauftragte für den Datenschutz und die Informationsfreiheit Rheinland-Pfalz
+            br
+            | LfDI
+            br
+            | Postfach 3040
+            br
+            | 55020 Mainz
 </template>
 
 <script>
